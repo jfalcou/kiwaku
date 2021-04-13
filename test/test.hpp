b0ed5cb6fecf7b83919e9a6c8df7c43086806933
Joel Falcou <joel.falcou@lri.fr>
2021-04-13 18:23:25 +0200

    Use latest TTS

--- test/test.hpp
+++ test/test.hpp
@@ -18,17 +18,6 @@ template<typename N> using up_to = std::make_index_sequence<N::value>;
 #define TTS_MAIN
 #define TTS_CUSTOM_DRIVER_FUNCTION kwk_entry_point
 #include <tts/tts.hpp>
-#include <kiwaku/allocator/bytes.hpp>
-
-namespace kwk
-{
-  inline std::string to_string(bytes b)
-  {
-    std::ostringstream str;
-    str << to_integer(b);
-    return str.str();
-  }
-}
 
 int main(int argc, char const **argv)
 {
