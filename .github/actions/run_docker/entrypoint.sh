9eb80f10297eb842d308131e2e3065008c4b1408
Joel Falcou <joel.falcou@lri.fr>
2022-03-01 13:08:30 +0100

    Add clang+libc++ CI target

--- .github/actions/run_docker/entrypoint.sh
+++ .github/actions/run_docker/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -l
 
-echo "::group::Running: 'cmake .. -G Ninja -DCMAKE_CXX_COMPILER="$1"'"
+echo "::group::Running: 'cmake .. -G Ninja -DCMAKE_CXX_COMPILER="$1" $3'"
 mkdir build
 cd build
-cmake .. -G Ninja -DCMAKE_CXX_COMPILER=$1 && ninja unit -j $2 && ctest -j $2
+cmake .. -G Ninja -DCMAKE_CXX_COMPILER=$1 && ninja unit -j $2 && ctest -j $2 $3
 echo "::endgroup::"
 
 exit 0
