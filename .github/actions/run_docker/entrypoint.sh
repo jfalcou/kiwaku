6d9c1cf8682c21c541eaa3de0cf64eeed698b7b0
Joel Falcou <joel.falcou@lri.fr>
2022-03-18 15:17:11 +0100

    Fix CI checks

--- .github/actions/run_docker/entrypoint.sh
+++ .github/actions/run_docker/entrypoint.sh
@@ -3,7 +3,20 @@
 echo "::group::Running: 'cmake .. -G Ninja -DCMAKE_CXX_COMPILER="$1" $3'"
 mkdir build
 cd build
-cmake .. -G Ninja -DCMAKE_CXX_COMPILER=$1 && ninja unit -j $2 && ctest -j $2 $3
+cmake .. -G Ninja -DCMAKE_CXX_COMPILER=$1 && ninja unit -j $2
+if [ "$?" -ne "0" ]
+then
+  echo "::error Tests can not be compiled!" ;
+  exit 1;
+fi
+
+ctest -j $2 $3
+if [ "$?" -ne "0" ]
+then
+  echo "::error Tests failed!" ;
+  exit 1;
+fi
+
 echo "::endgroup::"
 
 exit 0
