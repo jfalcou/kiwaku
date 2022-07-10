ab96cf04e9297ee5d40b8816b6388f415f87408b
Joel Falcou <joel.falcou@lri.fr>
2022-07-10 19:52:22 +0200

    Integration tests

--- cmake/kiwaku-config.cmake
+++ cmake/kiwaku-config.cmake
@@ -8,5 +8,5 @@
 ## Reuse install.cmake to prepare package properly
 ##==================================================================================================
 get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-include(${SELF_DIR}/kwk_lib.cmake)
-set(KWK_LIBRARIES kwk_lib)
+include(${SELF_DIR}/kiwaku_lib.cmake)
+set(KIWAKU_LIBRARIES kiwaku_lib)
--- cmake/kiwaku-install.cmake
+++ cmake/kiwaku-install.cmake
@@ -9,18 +9,18 @@ set(INSTALL_DEST  "include")
 ## =================================================================================================
 ## Exporting target for external use
 ## =================================================================================================
-add_library(kwk_lib INTERFACE)
-target_include_directories( kwk_lib INTERFACE
+add_library(kiwaku_lib INTERFACE)
+target_include_directories( kiwaku_lib INTERFACE
                             $<BUILD_INTERFACE:${PROJECT_SOURCE_DIR}/include>
                             $<INSTALL_INTERFACE:${INSTALL_DEST}>
                           )
-target_compile_features(kwk_lib INTERFACE cxx_std_20)
-add_library(kwk::kwk ALIAS kwk_lib)
+target_compile_features(kiwaku_lib INTERFACE cxx_std_20)
+add_library(kiwaku::kiwaku ALIAS kiwaku_lib)
 
 ## =================================================================================================
 ## Install target with versionned folder
 ## =================================================================================================
-install(TARGETS   kwk_lib EXPORT kwk_lib  DESTINATION "${MAIN_DEST}")
+install(TARGETS   kiwaku_lib EXPORT kiwaku_lib                    DESTINATION "${MAIN_DEST}"    )
 install(DIRECTORY ${PROJECT_SOURCE_DIR}/include/kwk               DESTINATION "${INSTALL_DEST}" )
-install(FILES     ${PROJECT_SOURCE_DIR}/cmake/kwk-config.cmake    DESTINATION "${MAIN_DEST}"    )
-install(EXPORT    kwk_lib DESTINATION "${MAIN_DEST}")
+install(FILES     ${PROJECT_SOURCE_DIR}/cmake/kiwaku-config.cmake DESTINATION "${MAIN_DEST}"    )
+install(EXPORT    kiwaku_lib DESTINATION "${MAIN_DEST}")
