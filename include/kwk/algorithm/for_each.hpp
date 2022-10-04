cadae54d5c398f9237818c78da64558cbdd48ee6
Joel Falcou <joel.falcou@lri.fr>
2022-10-04 22:55:02 +0200

    Basic for_each

--- include/kwk/algorithm/for_each.hpp
+++ include/kwk/algorithm/for_each.hpp
@@ -7,6 +7,7 @@
 //==================================================================================================
 #pragma once
 
+#include <kwk/detail/kumi.hpp>
 #include <cstddef>
 #include <utility>
 
