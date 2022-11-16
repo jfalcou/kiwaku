fc6ba61df6665d668e68cacc77d1ae10ca2b79e9
Joel Falcou <joel.falcou@lri.fr>
2022-11-16 14:46:53 +0100

    Run CI on Debug+Release

--- .github/actions/run_docker/entrypoint.sh
+++ .github/actions/run_docker/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -l
 
 set -e
-echo "::group::Running: 'cmake .. -G Ninja -DCMAKE_CXX_COMPILER="$1" $3'"
+echo "::group::Running: 'cmake .. -G Ninja -DCMAKE_CXX_COMPILER=$1 -DCMAKE_CXX_FLAGS="$3"  -DCMAKE_EXE_LINKER_FLAGS="$4"'"
 mkdir build
 cd build
-cmake .. -G Ninja -DCMAKE_CXX_COMPILER=$1 $3
+cmake .. -G Ninja -DCMAKE_CXX_COMPILER=$1 -DCMAKE_CXX_FLAGS="$3"  -DCMAKE_EXE_LINKER_FLAGS="$4"
 echo "::endgroup::"
 
 echo "::group::Compiling tests"
