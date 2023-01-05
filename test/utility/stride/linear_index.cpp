a23fb04415a86d3b660ef5a8168213c6e227f3d1
Joel Falcou <joel.falcou@lri.fr>
2023-01-05 20:29:18 +0100

    Fix stride unit tests

--- test/utility/stride/linear_index.cpp
+++ test/utility/stride/linear_index.cpp
@@ -25,13 +25,13 @@ TTS_CASE( "2D behavior of linear_index" )
 {
   kwk::shape sh{4,3};
 
-  for(int i1 = 0; i1 < 3; ++i1)
+  for(int i1 = 0; i1 < 4; ++i1)
   {
-    for(int i0 = 0; i0 < 4; ++i0)
+    for(int i0 = 0; i0 < 3; ++i0)
     {
-      TTS_EQUAL((i0+4*i1), (kwk::linear_index(sh, i0, i1)) );
-      TTS_EQUAL((i0+4*i1), (kwk::linear_index(sh,kumi::tuple{i0,i1})) );
-      TTS_EQUAL((i0+4*i1), (kwk::linear_index(sh,std::array<int,2>{i0,i1})) );
+      TTS_EQUAL((i0+3*i1), (kwk::linear_index(sh, i0, i1)) );
+      TTS_EQUAL((i0+3*i1), (kwk::linear_index(sh,kumi::tuple{i0,i1})) );
+      TTS_EQUAL((i0+3*i1), (kwk::linear_index(sh,std::array<int,2>{i0,i1})) );
     }
   }
 };
@@ -40,15 +40,15 @@ TTS_CASE( "3D behavior of linear_index" )
 {
   kwk::shape sh{4,3,2};
 
-  for(int i2 = 0; i2 < 2; ++i2)
+  for(int i2 = 0; i2 < 4; ++i2)
   {
     for(int i1 = 0; i1 < 3; ++i1)
     {
-      for(int i0 = 0; i0 < 4; ++i0)
+      for(int i0 = 0; i0 < 2; ++i0)
       {
-        TTS_EQUAL((i0+4*i1+12*i2), (kwk::linear_index(sh, i0, i1, i2)) );
-        TTS_EQUAL((i0+4*i1+12*i2), (kwk::linear_index(sh,kumi::tuple{i0,i1,i2})) );
-        TTS_EQUAL((i0+4*i1+12*i2), (kwk::linear_index(sh,std::array<int,3>{i0,i1,i2})) );
+        TTS_EQUAL((i0+2*i1+6*i2), (kwk::linear_index(sh, i0, i1, i2)) );
+        TTS_EQUAL((i0+2*i1+6*i2), (kwk::linear_index(sh,kumi::tuple{i0,i1,i2})) );
+        TTS_EQUAL((i0+2*i1+6*i2), (kwk::linear_index(sh,std::array<int,3>{i0,i1,i2})) );
       }
     }
   }
@@ -58,17 +58,17 @@ TTS_CASE( "4D behavior of linear_index" )
 {
   kwk::shape sh{5,4,3,2};
 
-  for(int i3 = 0; i3 < 2; ++i3)
+  for(int i3 = 0; i3 < 5; ++i3)
   {
-    for(int i2 = 0; i2 < 3; ++i2)
+    for(int i2 = 0; i2 < 4; ++i2)
     {
-      for(int i1 = 0; i1 < 4; ++i1)
+      for(int i1 = 0; i1 < 3; ++i1)
       {
-        for(int i0 = 0; i0 < 5; ++i0)
+        for(int i0 = 0; i0 < 2; ++i0)
         {
-          TTS_EQUAL((i0+5*i1+20*i2+60*i3), (kwk::linear_index(sh, i0, i1, i2, i3)) );
-          TTS_EQUAL((i0+5*i1+20*i2+60*i3), (kwk::linear_index(sh,kumi::tuple{i0,i1,i2,i3})) );
-          TTS_EQUAL((i0+5*i1+20*i2+60*i3), (kwk::linear_index(sh,std::array<int,4>{i0,i1,i2,i3})) );
+          TTS_EQUAL((i0+2*i1+6*i2+24*i3), (kwk::linear_index(sh, i0, i1, i2, i3)) );
+          TTS_EQUAL((i0+2*i1+6*i2+24*i3), (kwk::linear_index(sh,kumi::tuple{i0,i1,i2,i3})) );
+          TTS_EQUAL((i0+2*i1+6*i2+24*i3), (kwk::linear_index(sh,std::array<int,4>{i0,i1,i2,i3})) );
         }
       }
     }
