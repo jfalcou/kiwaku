9d57ff89bb5a3acb7aaa146c9151c0386af2bfec
Joel Falcou <joel.falcou@lri.fr>
2023-01-17 17:06:09 +0100

    Provides an abstraction for data axis for shape

---
+++ include/kwk/concepts/axis.hpp
@@ -0,0 +1,41 @@
+//==================================================================================================
+/*
+  KIWAKU - Containers Well Made
+  Copyright : KIWAKU Project Contributors
+  SPDX-License-Identifier: BSL-1.0
+*/
+//==================================================================================================
+#pragma once
+
+#include <concepts>
+
+namespace kwk::concepts
+{
+  //================================================================================================
+  //! @brief Axis concept
+  //!
+  //! A **KIWAKU** Axis is a type is usable as a shape descriptor component
+  //================================================================================================
+  template<typename T>
+  concept axis = requires(T)
+  {
+    typename T::axis_kind;
+    { std::integral_constant<std::ptrdiff_t, T::static_index>{} };
+  };
+
+  //================================================================================================
+  //! @brief Dynamically-sized axis concept
+  //!
+  //! A **KIWAKU** Dynamic Axis is an Axis type which size will be set at runtime.
+  //================================================================================================
+  template<typename T>
+  concept dynamic_axis = axis<T> && !std::integral<typename T::value_type>;
+
+  //================================================================================================
+  //! @brief Statically-sized axis concept
+  //!
+  //! A **KIWAKU** Static Axis is an Axis type which size will be set at compile-time.
+  //================================================================================================
+  template<typename T>
+  concept static_axis = axis<T> && std::integral<typename T::value_type>;
+}
---
+++ include/kwk/detail/traits.hpp
@@ -0,0 +1,52 @@
+//==================================================================================================
+/*
+  KIWAKU - Containers Well Made
+  Copyright : KIWAKU Project Contributors
+  SPDX-License-Identifier: BSL-1.0
+*/
+//==================================================================================================
+#pragma once
+
+#include <cstddef>
+#include <utility>
+
+namespace kwk::detail
+{
+  //================================================================================================
+  // Check if a given list of types is a set
+  // To do so, we build a type that linearly inherits from box<N,T>... and we check that
+  // there is no ambiguity when trying to convert a bunch of Ts... into a block of inheritance
+  //================================================================================================
+  template<typename T> struct box {};
+  template <std::size_t, typename T> struct unique { operator T(); };
+
+  template <typename Ints, typename... Ts>
+  struct all_unique_impl;
+
+  template <>
+  struct all_unique_impl<std::index_sequence<>> { using type = std::true_type; };
+
+  template <std::size_t... Ints, typename... Ts>
+  struct all_unique_impl<std::index_sequence<Ints...>, Ts...>
+  {
+    static inline std::true_type detect(...);
+    struct all_uniques : unique<Ints, Ts>... {};
+
+    template <typename... Us>
+    static auto check(Us...) -> decltype(detect(static_cast<Us>(all_uniques())...));
+    static std::false_type check(...);
+
+    using type = decltype(check(Ts{}...));
+  };
+
+  template<typename... Ts>
+  concept all_unique = all_unique_impl<std::index_sequence_for<Ts...>, box<Ts>...>::type::value;
+
+  //================================================================================================
+  // Check two axis are of the same kind
+  //================================================================================================
+  template<typename T, typename U>
+  inline constexpr bool is_along_v  = std::same_as< typename T::axis_kind
+                                                  , typename U::axis_kind
+                                                  >;
+}
