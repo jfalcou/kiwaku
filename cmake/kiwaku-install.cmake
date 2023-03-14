8b97ff2f1d3d0497ae9a6702c788e1fe0b484a9c
Joel Falcou <joel.falcou@lri.fr>
2023-03-14 14:07:22 +0100

    Prepare better integration tests

--- cmake/kiwaku-install.cmake
+++ cmake/kiwaku-install.cmake
@@ -1,10 +1,12 @@
 ##==================================================================================================
-##  KIWAKU - Containers Well Made
-##  Copyright : KIWAKU Contributors & Maintainers
+##  EVE - Expressive Vector Engine
+##  Copyright : EVE Project Contributors
 ##  SPDX-License-Identifier: BSL-1.0
 ##==================================================================================================
-set(MAIN_DEST     "lib")
-set(INSTALL_DEST  "include")
+include(GNUInstallDirs)
+set(MAIN_DEST     "${CMAKE_INSTALL_LIBDIR}/kiwaku-${PROJECT_VERSION}")
+set(INSTALL_DEST  "${CMAKE_INSTALL_INCLUDEDIR}/kiwaku-${PROJECT_VERSION}")
+set(DOC_DEST      "${CMAKE_INSTALL_DOCDIR}-${PROJECT_VERSION}")
 
 ## =================================================================================================
 ## Exporting target for external use
@@ -15,12 +17,14 @@ target_include_directories( kiwaku_lib INTERFACE
                             $<INSTALL_INTERFACE:${INSTALL_DEST}>
                           )
 target_compile_features(kiwaku_lib INTERFACE cxx_std_20)
+set_target_properties(kiwaku_lib PROPERTIES EXPORT_NAME kiwaku)
 add_library(kiwaku::kiwaku ALIAS kiwaku_lib)
 
 ## =================================================================================================
-## Install target with versionned folder
+## Install target with versioned folder
 ## =================================================================================================
-install(TARGETS   kiwaku_lib EXPORT kiwaku_lib                    DESTINATION "${MAIN_DEST}"    )
+install(TARGETS   kiwaku_lib EXPORT kiwaku-targets DESTINATION "${MAIN_DEST}")
 install(DIRECTORY ${PROJECT_SOURCE_DIR}/include/kwk               DESTINATION "${INSTALL_DEST}" )
-install(FILES     ${PROJECT_SOURCE_DIR}/cmake/kiwaku-config.cmake DESTINATION "${MAIN_DEST}"    )
-install(EXPORT    kiwaku_lib DESTINATION "${MAIN_DEST}")
+install(FILES     ${PROJECT_SOURCE_DIR}/cmake/kiwaku-config.cmake    DESTINATION "${MAIN_DEST}"    )
+install(FILES     ${PROJECT_SOURCE_DIR}/LICENSE.md                DESTINATION "${DOC_DEST}"     )
+install(EXPORT    kiwaku-targets NAMESPACE "kiwaku::" DESTINATION "${MAIN_DEST}")
