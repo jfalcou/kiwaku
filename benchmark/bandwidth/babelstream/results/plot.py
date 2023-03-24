a0d35216167a673abb231583aa8c314648b0c099
higepi <sasa.radosavljevic@outlook.fr>
2023-03-24 09:14:19 +0100

    figures float & double

--- benchmark/bandwidth/babelstream/results/plot.py
+++ benchmark/bandwidth/babelstream/results/plot.py
@@ -1,45 +1,40 @@
 import numpy as np
-import re
+import pandas as pd
 from matplotlib import pyplot as plt
+import sys
 
-with open ('babelstream.txt', 'r') as results:
-    text = results.read()
+def Parse(fname, df, Function, metric):
+  copy = df.loc[df['Function'] == Function]
+  plot(fname, copy, Function, metric)
 
-# extract the sizes values
-values = re.findall(r'Array size :: (\d+)*', text)
-sizes = [float(value) if '.' in value else int(value) for value in values]
+def plot(fname, df, Function, metric):
+  plt.figure()
+  plt.semilogx(df['Size(Bytes)'], df[metric])
+  plt.xlabel('Size (Bytes)')
+  plt.ylabel('GBytes/s')
+  plt.title(fname.split(sep='.')[0]+ "_" + Function)
+  plt.legend(metric)
+  plt.savefig(fname.split(sep='.')[0]+ "_" + Function)
+  # plt.show()
 
-# extract the copy bandwidth
-values = re.findall(r"\d+\.\d+", text)
-values = [float(value) for value in values]
+def main():
+  n_arg = len(sys.argv)
 
-print(values[0])
-# result = np.reshape(values,(35,5))
+  if(n_arg < 2):
+    print("Please input the file\n")
+    return -1
+  else:
+    for Arg in sys.argv[1:]:
+      fname = Arg
 
-# SEQ = result[0:5,3]
+      df = pd.read_csv(fname, sep=";")
+      Parse(fname, df, 'Copy', ['Mean Babel(GBytes/sec)','Mean Nano(GBytes/sec)'])
+      Parse(fname, df, 'Mul', ['Mean Babel(GBytes/sec)','Mean Nano(GBytes/sec)'])
+      Parse(fname, df, 'Add', ['Mean Babel(GBytes/sec)','Mean Nano(GBytes/sec)'])
+      Parse(fname, df, 'Triad', ['Mean Babel(GBytes/sec)','Mean Nano(GBytes/sec)'])
+      Parse(fname, df, 'Dot', ['Mean Babel(GBytes/sec)','Mean Nano(GBytes/sec)'])
+      Parse(fname, df, 'Nstream', ['Mean Babel(GBytes/sec)','Mean Nano(GBytes/sec)'])
+   
+if __name__ == "__main__":
+  sys.exit(main())
 
-# result = np.delete(result, (0,2,3), 1)
-
-# T1 = result[0:5,:]
-# T2 = result[5:10,:]
-# T4 = result[10:15,:]
-# T6 = result[15:20,:]
-# T8 = result[20:25,:]
-# T10 = result[25:30,:]
-# T12 = result[30:35,:]
-
-# plt.figure()
-# plt.plot(T1[:,0],SEQ,'+-')
-# plt.plot(T1[:,0],T1[:,1],'+-')
-# plt.plot(T2[:,0],T2[:,1],'+-')
-# plt.plot(T4[:,0],T4[:,1],'+-')
-# plt.plot(T6[:,0],T6[:,1],'+-')
-# plt.plot(T8[:,0],T8[:,1],'+-')
-# plt.plot(T10[:,0],T10[:,1],'+-')
-# plt.plot(T12[:,0],T12[:,1],'+-')
-
-# plt.legend(["Sequentiel","1 Thread","2 Threads","4 Threads","6 Threads","8 Threads","10 Threads","12 Threads"])
-# plt.xlabel("N")
-# plt.ylabel("t (s)")
-# plt.title("Multiplication")
-# plt.show()
