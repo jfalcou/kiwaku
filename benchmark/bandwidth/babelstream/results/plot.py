487c1a063e56000050d363a77a82e820c895ba62
higepi <sasa.radosavljevic@outlook.fr>
2023-03-27 14:33:36 +0200

    soon box

--- benchmark/bandwidth/babelstream/results/plot.py
+++ benchmark/bandwidth/babelstream/results/plot.py
@@ -3,22 +3,74 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import sys
 
-def Parse(fname, df, Function, metric):
-  copy = df.loc[df['Function'] == Function]
-  plot(fname, copy, Function, metric)
+def Parse(fname, df, Function, metric, plot_type, save):
+    for f in Function:
+      copy = df.loc[df['Function'] == f]
 
-def plot(fname, df, Function, metric):
+      if(fname.__contains__('nano')):
+        if("plot" in plot_type):
+          plot(fname, copy, f, metric, save)
+        if("bar" in plot_type):
+          box(fname, copy, f, metric, save)
+
+      if(fname.__contains__('chrono')):
+        box_chrono(fname, df, save)
+      
+def plot(fname, df, Function, metric, save):
   plt.figure()
   plt.semilogx(df['Size(Bytes)'], df[metric])
   plt.xlabel('Size (Bytes)')
   plt.ylabel('GBytes/s')
   plt.title(fname.split(sep='.')[0]+ "_" + Function)
   plt.legend(metric)
-  plt.savefig(fname.split(sep='.')[0]+ "_" + Function)
-  # plt.show()
 
+  if save:
+    plt.savefig(fname.split(sep='.')[0]+ "_" + Function)
+  else:
+    plt.show()
+
+def box(fname, df, Function, metric, save):
+    means = df['Mean Nano(GBytes/sec)']
+    mins = df['Min Nano(GBytes/sec)']
+    maxs = df['Max Nano(GBytes/sec)']
+    errors = df['Err Nano(GBytes/sec)']
+    x = df['Size(Bytes)']
+
+    plt.figure()
+    plt.xscale('log')
+    plt.errorbar(x, means, errors, fmt='.k', lw=2)
+    plt.errorbar(x, means, [abs(means-mins), abs(maxs-means)], fmt='.k', lw=1)
+    plt.xlabel('Size (Bytes)')
+    plt.ylabel('GBytes/s')
+    plt.title(fname.split(sep='.')[0]+ "_" + Function)
+
+    if save:
+      plt.savefig(fname.split(sep='.')[0]+ "_" + Function + "_bars")
+    else:
+      plt.show()
+
+def box_chrono(fname, df, save):
+  x = df['Size(Bytes)'].to_numpy()
+  values = df.iloc[:,3:].to_numpy()
+
+  toto = str(x[0])
+  print(toto)
+
+  print(values[0])
+  plt.figure()
+  plt.boxplot(values)
+  # plt.xlabel(toto)
+  plt.xscale('log')
+
+  
+  plt.show()
+  
 def main():
   n_arg = len(sys.argv)
+
+  functions = ['Copy', 'Mul', 'Add','Triad','Dot','Nstream']
+  metric = ['Mean Babel(GBytes/sec)','Mean Nano(GBytes/sec)','Median Nano(GBytes/sec)']
+  plot_type = ['plot','bar']
 
   if(n_arg < 2):
     print("Please input the file\n")
@@ -26,14 +78,8 @@ def main():
   else:
     for Arg in sys.argv[1:]:
       fname = Arg
-
       df = pd.read_csv(fname, sep=";")
-      Parse(fname, df, 'Copy', ['Mean Babel(GBytes/sec)','Mean Nano(GBytes/sec)','Median Nano(GBytes/sec)'])
-      Parse(fname, df, 'Mul', ['Mean Babel(GBytes/sec)','Mean Nano(GBytes/sec)','Median Nano(GBytes/sec)'])
-      Parse(fname, df, 'Add', ['Mean Babel(GBytes/sec)','Mean Nano(GBytes/sec)','Median Nano(GBytes/sec)'])
-      Parse(fname, df, 'Triad', ['Mean Babel(GBytes/sec)','Mean Nano(GBytes/sec)','Median Nano(GBytes/sec)'])
-      Parse(fname, df, 'Dot', ['Mean Babel(GBytes/sec)','Mean Nano(GBytes/sec)','Median Nano(GBytes/sec)'])
-      Parse(fname, df, 'Nstream', ['Mean Babel(GBytes/sec)','Mean Nano(GBytes/sec)','Median Nano(GBytes/sec)'])
+      Parse(fname, df, functions, metric, plot_type, save=False)
    
 if __name__ == "__main__":
   sys.exit(main())
--- benchmark/readme.md
+++ benchmark/readme.md
@@ -24,16 +24,4 @@ We use boxplot as a detailed statistical method to evaluate the difference in ba
 
 ## Nanobench
 
-We use Nanobench as a comparison to std::chrono for the cycles measurements :
-```
-ankerl::nanobench::Bench bcopy;
-
-bcopy.epochIterations(num_times).epochs(1).run("Copy", [&]{
-  copy(a, c);
-});
-
-std::vector<ankerl::nanobench::Result> vres;
-vres = bcopy.results();
-double cyc_op = vres.begin()->median(ankerl::nanobench::Result::Measure::cpucycles);
-std::cout << "Resultat nanobench mult: " << cyc_op << std::endl;
-```
+We use Nanobench as a comparison to std::chrono as it gives the cycles measurements.
