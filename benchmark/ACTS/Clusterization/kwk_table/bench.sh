dac5a07b872d7c2c02bfe7b6088b801fe004fe40
higepi <sasa.radosavljevic@outlook.fr>
2023-04-27 15:38:41 +0200

    bench ccl find connections

---
+++ benchmark/ACTS/Clusterization/kwk_table/bench.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+path=`pwd`
+
+cd $path/build/
+cmake ../cmake -DCPM_kiwaku_SOURCE=/home/higepi/stage/kiwaku -G Ninja -DCMAKE_CXX_COMPILER=clang++-15
+ninja -v Cluster.out
+
+cd $path/results
+
+# for i in 2 4 10 20 50 100
+# do
+#   ../build/Cluster.out $i
+# done
+
+##debug
+
+../build/Cluster.out 4
