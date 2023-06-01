ef23b313624f6536237beb21bc75fae67e586cce
higepi <sasa.radosavljevic@outlook.fr>
2023-06-01 11:47:38 +0200

    kmeans 1D std

---
+++ benchmark/algorithms/bsearch/kwk_view/bsearch_kwk_view.cpp
@@ -0,0 +1,17 @@
+#include "bsearch_kwk_view.hpp"
+
+int main(int argc, char *argv[])
+{
+  parseArguments(argc, argv);
+
+  if(BENCHMARK)
+  {
+    Benchmarking<float>();
+    Benchmarking<double>();
+  } 
+  else 
+  {
+    run<float>();
+    run<double>();
+  }
+}
