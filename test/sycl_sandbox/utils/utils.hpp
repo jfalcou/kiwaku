bc775aa3747ec750f2bec5948b93e3b41411856a
Sylvain Joube <sylvain.joube@gmail.com>
2024-02-28 11:24:02 +0100

    Remove SYCL POC

--- test/sycl_sandbox/utils/utils.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "global_file.hpp"
-#include "printer.hpp"
-#include "text_file.hpp"
-#include "chrono.hpp"
-#include "host_name.hpp"
-
-const bool G_PRINT_LORENTZ_CHK = false;
