368e8968854ee3afffa031bda44174070122ec75
Sylvain Joube <sylvain.joube@gmail.com>
2024-05-02 21:51:30 +0200

    WIP binary_search test: remove size 0

--- test/algorithm/algos/binary_search.cpp
+++ test/algorithm/algos/binary_search.cpp
@@ -147,32 +147,32 @@ TTS_CASE("Check for kwk::lower_bound(In, value, func) 1D with function")
   // std::cout << (*kwk::lower_bound(view, 21))[0] << std::endl;
 };
 
-TTS_CASE("Check for kwk::lower_bound 1D: size 0")
-{
-  const std::size_t input_size = 0;
-  std::array<int, input_size> input;
-  auto view = kwk::view{kwk::source = input, kwk::of_size(input_size)};
-  auto func = [](auto const& input_, auto const& element_) { return input_ < (element_ - 10) * 2; };
+// TTS_CASE("Check for kwk::lower_bound 1D: size 0")
+// {
+//   const std::size_t input_size = 0;
+//   std::array<int, input_size> input;
+//   auto view = kwk::view{kwk::source = input, kwk::of_size(input_size)};
+//   auto func = [](auto const& input_, auto const& element_) { return input_ < (element_ - 10) * 2; };
 
-  LB_ERROR(input, view, -2984612);
-  LB_ERROR(input, view, -2);
-  LB_ERROR(input, view, 0);
-  LB_ERROR(input, view, 5);
-  LB_ERROR(input, view, 19);
-  LB_ERROR(input, view, 20);
-  LB_ERROR(input, view, 21);
-  LB_ERROR(input, view, 78456465);
+//   LB_ERROR(input, view, -2984612);
+//   LB_ERROR(input, view, -2);
+//   LB_ERROR(input, view, 0);
+//   LB_ERROR(input, view, 5);
+//   LB_ERROR(input, view, 19);
+//   LB_ERROR(input, view, 20);
+//   LB_ERROR(input, view, 21);
+//   LB_ERROR(input, view, 78456465);
 
-  LB_ERROR_FUNC(input, view, func, -2984612);
-  LB_ERROR_FUNC(input, view, func, -2);
-  LB_ERROR_FUNC(input, view, func, 0);
-  LB_ERROR_FUNC(input, view, func, 5);
-  LB_ERROR_FUNC(input, view, func, 9);
-  LB_ERROR_FUNC(input, view, func, 19);
-  LB_ERROR_FUNC(input, view, func, 20);
-  LB_ERROR_FUNC(input, view, func, 21);
-  LB_ERROR_FUNC(input, view, func, 78456465);
-};
+//   LB_ERROR_FUNC(input, view, func, -2984612);
+//   LB_ERROR_FUNC(input, view, func, -2);
+//   LB_ERROR_FUNC(input, view, func, 0);
+//   LB_ERROR_FUNC(input, view, func, 5);
+//   LB_ERROR_FUNC(input, view, func, 9);
+//   LB_ERROR_FUNC(input, view, func, 19);
+//   LB_ERROR_FUNC(input, view, func, 20);
+//   LB_ERROR_FUNC(input, view, func, 21);
+//   LB_ERROR_FUNC(input, view, func, 78456465);
+// };
 
 
 TTS_CASE("Check for kwk::upper_bound(In, value) 1D ---------")
@@ -215,35 +215,35 @@ TTS_CASE("Check for kwk::upper_bound(In, value) 1D with function")
   UB_ERROR_FUNC(input, view, func, 78456465);
 };
 
-TTS_CASE("Check for kwk::upper_bound 1D: size = 0")
-{
-  const std::size_t input_size = 0;
-  std::array<int, input_size> input;
-  for (std::size_t i = 0; i < input_size; ++i) { input[i] = i * 3 - 20; }
-  auto view = kwk::view{kwk::source = input, kwk::of_size(input_size)};
-  auto func = [](auto const& search_, auto const& element_) { return search_ * 3 - 20 < element_; };
+// TTS_CASE("Check for kwk::upper_bound 1D: size = 0")
+// {
+//   const std::size_t input_size = 0;
+//   std::array<int, input_size> input;
+//   for (std::size_t i = 0; i < input_size; ++i) { input[i] = i * 3 - 20; }
+//   auto view = kwk::view{kwk::source = input, kwk::of_size(input_size)};
+//   auto func = [](auto const& search_, auto const& element_) { return search_ * 3 - 20 < element_; };
 
-  UB_ERROR_FUNC(input, view, func, -895);
-  UB_ERROR_FUNC(input, view, func, -78495);
-  UB_ERROR_FUNC(input, view, func, -1);
-  UB_ERROR_FUNC(input, view, func, 0);
-  UB_ERROR_FUNC(input, view, func, 5);
-  UB_ERROR_FUNC(input, view, func, 18);
-  UB_ERROR_FUNC(input, view, func, 19);
-  UB_ERROR_FUNC(input, view, func, 2000);
-  UB_ERROR_FUNC(input, view, func, 78456465);
+//   UB_ERROR_FUNC(input, view, func, -895);
+//   UB_ERROR_FUNC(input, view, func, -78495);
+//   UB_ERROR_FUNC(input, view, func, -1);
+//   UB_ERROR_FUNC(input, view, func, 0);
+//   UB_ERROR_FUNC(input, view, func, 5);
+//   UB_ERROR_FUNC(input, view, func, 18);
+//   UB_ERROR_FUNC(input, view, func, 19);
+//   UB_ERROR_FUNC(input, view, func, 2000);
+//   UB_ERROR_FUNC(input, view, func, 78456465);
 
-  UB_ERROR(input, view, -895);
-  UB_ERROR(input, view, -78495);
-  UB_ERROR(input, view, -1);
-  UB_ERROR(input, view, 0);
-  UB_ERROR(input, view, 5);
-  UB_ERROR(input, view, 18);
-  UB_ERROR(input, view, 19);
-  UB_ERROR(input, view, 21);
-  UB_ERROR(input, view, 2000);
-  UB_ERROR(input, view, 78456465);
-};
+//   UB_ERROR(input, view, -895);
+//   UB_ERROR(input, view, -78495);
+//   UB_ERROR(input, view, -1);
+//   UB_ERROR(input, view, 0);
+//   UB_ERROR(input, view, 5);
+//   UB_ERROR(input, view, 18);
+//   UB_ERROR(input, view, 19);
+//   UB_ERROR(input, view, 21);
+//   UB_ERROR(input, view, 2000);
+//   UB_ERROR(input, view, 78456465);
+// };
 
 TTS_CASE("Check for kwk::binary_search(In, value) 1D ---------")
 {
