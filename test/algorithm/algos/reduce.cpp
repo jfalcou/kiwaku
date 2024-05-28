8cd5f372db55f11b57db2b9927274d3414e76e45
Sylvain Joube <sylvain.joube@gmail.com>
2024-05-28 13:53:37 +0200

    Tests reduce: 1D

--- test/algorithm/algos/reduce.cpp
+++ test/algorithm/algos/reduce.cpp
@@ -9,347 +9,43 @@
 #include <kwk/algorithm/algos/reduce.hpp>
 #include <kwk/container.hpp>
 #include "test.hpp"
+#include <numeric>
 
 TTS_CASE("Check for kwk::reduce(in) 1D")
 {
-  int data[2] = {1,2};
-  int vdata = 3;
+  using data_type = int;
+  const std::size_t d0 = 471;
+  const std::size_t input_size = d0;
+  std::array<data_type, input_size> input;
 
-  auto d = kwk::view{kwk::source = data};
-  auto res = kwk::reduce(d);
+  for (std::size_t i = 0; i < input_size; ++i) { input[i] = i * 3; }
 
-  TTS_EQUAL(res, vdata);
+  auto view_in  = kwk::view{kwk::source = input , kwk::of_size(d0)};
+
+  auto res = kwk::reduce(view_in);
+  auto res_std = std::reduce(input.begin(), input.end());
+
+  TTS_EQUAL(res, res_std);
 };
 
-TTS_CASE("Check for kwk::reduce(in) 1D - larger")
+
+TTS_CASE("Check for kwk::reduce(in, func) and kwk::reduce(in, func, init) 1D")
 {
-  const std::size_t size = 1600000;
-  double data[size];
-
-  auto global_fct = [](auto init, auto x) { return init + x; };
-
-  double sum = 0;
-  for (std::size_t i = 0; i < size; ++i) { data[i] = i; sum = global_fct(sum, i); }
-
-  auto d = kwk::view{kwk::source = data, kwk::of_size(size)};
-
-  auto res = kwk::reduce(d, global_fct);
-
-  std::cout << res << "\n";
-
-  TTS_EQUAL(res, sum);
-};
-
-TTS_CASE("Check for kwk::reduce(in) 2D")
-{
-  int data[2*3];
-  int vdata = 36;
-
-  fill_data(data, kwk::of_size(2,3), true);
-
-  auto d = kwk::view{kwk::source = data, kwk::of_size(2,3)};
-
-  auto res = kwk::reduce(d);
-
-  TTS_EQUAL(res, vdata);
-};
-
-TTS_CASE("Check for kwk::reduce(in) 2D - with CPU context")
-{
-  int data[2*3];
-  int vdata = 36;
-
-  fill_data(data, kwk::of_size(2,3), true);
-
-  auto d = kwk::view{kwk::source = data, kwk::of_size(2,3)};
-
-  auto res = kwk::reduce(kwk::cpu, d);
-
-  TTS_EQUAL(res, vdata);
-};
-
-TTS_CASE("Check for kwk::reduce(in) 3D")
-{
-  int data[2*3*4];
-  int vdata = 1476;
-
-  fill_data(data, kwk::of_size(2,3,4), true);
-
-  auto d = kwk::view{kwk::source = data, kwk::of_size(2,3,4)};
-
-  auto res = kwk::reduce(d);
-
-  TTS_EQUAL(res, vdata);
-};
-
-TTS_CASE("Check for kwk::reduce(in) 4D")
-{
-  int data[2*3*4*5];
-  int vdata = 74040;
-
-  fill_data(data, kwk::of_size(2,3,4,5), true);
-
-  auto d = kwk::view{kwk::source = data, kwk::of_size(2,3,4,5)};
-
-  auto res = kwk::reduce(d);
-
-  TTS_EQUAL(res, vdata);
-};
-
-TTS_CASE("Check for kwk::reduce(in, func) 1D")
-{
-  int data[2];
-  int vdata = 10;
-
-  fill_data(data, kwk::of_size(2), true);
-
-  auto d = kwk::view{kwk::source = data, kwk::of_size(2)};
-
-  int count = 0;
-  auto res = reduce(d,
-  [&count](auto a, auto e)
-  {
-    count++;
-    return (a+10*e);
-  });
-
-  TTS_EQUAL(res,   vdata);
-  TTS_EQUAL(count,   d.numel());
-};
-
-TTS_CASE("Check for kwk::reduce(in, func) 2D")
-{
-  int data[2*3];
-  int vdata = 360;
-
-  fill_data(data, kwk::of_size(2,3), true);
-
-  auto d = kwk::view{kwk::source = data, kwk::of_size(2,3)};
-
-  int count = 0;
-  auto res = reduce(d,
-  [&count](auto a, auto e)
-  {
-    count++;
-    return (a+10*e);
-  });
-
-  TTS_EQUAL(res,   vdata);
-  TTS_EQUAL(count,   d.numel());
-};
-
-TTS_CASE("Check for kwk::reduce(in, func) 2D - with CPU context")
-{
-  int data[2*3];
-  int vdata = 360;
-
-  fill_data(data, kwk::of_size(2,3), true);
-
-  auto d = kwk::view{kwk::source = data, kwk::of_size(2,3)};
-
-  int count = 0;
-  auto res = reduce(kwk::cpu, d,
-  [&count](auto a, auto e)
-  {
-    count++;
-    return (a+10*e);
-  });
-
-  TTS_EQUAL(res,   vdata);
-  TTS_EQUAL(count,   d.numel());
-};
-
-TTS_CASE("Check for kwk::reduce(in, func) 3D")
-{
-  int data[2*3*4];
-  int vdata = 14760;
-
-  fill_data(data, kwk::of_size(2,3,4), true);
-
-  auto d = kwk::view{kwk::source = data, kwk::of_size(2,3,4)};
-
-  int count = 0;
-  auto res = reduce(d,
-  [&count](auto a, auto e)
-  {
-    count++;
-    return (a+10*e);
-  });
-
-  TTS_EQUAL(res,   vdata);
-  TTS_EQUAL(count,   d.numel());
-};
-
-TTS_CASE("Check for kwk::reduce(in, func) 4D")
-{
-  int data[2*3*4*5];
-  int vdata = 740400;
-
-  fill_data(data, kwk::of_size(2,3,4,5), true);
-
-  auto d = kwk::view{kwk::source = data, kwk::of_size(2,3,4,5)};
-
-  int count = 0;
-  auto res = reduce(d,
-  [&count](auto a, auto e)
-  {
-    count++;
-    return (a+10*e);
-  });
-
-  TTS_EQUAL(res,   vdata);
-  TTS_EQUAL(count,   d.numel());
-};
-
-TTS_CASE("Check for float kwk::reduce(in, func)")
-{
-  float data[2*2]      = { 1.f,2.2f
-                          , 3.3f,4.4f
-                          };
-
-  float vdata        =  10.9f;
-
-  auto d = kwk::view{kwk::source = data, kwk::of_size(2,2)};
-
-
-  int count = 0;
-  auto res = reduce(d,
-  [&count](auto a, auto e)
-  {
-    count++;
-    return (a+e);
-  });
-
-  TTS_EQUAL(res,   vdata);
-
-  TTS_EQUAL(count,   d.numel());
-};
-
-TTS_CASE("Check for float kwk::reduce(in, func) - with CPU context")
-{
-  float data[2*2]      = { 1.f,2.2f
-                          , 3.3f,4.4f
-                          };
-
-  float vdata        =  10.9f;
-
-  auto d = kwk::view{kwk::source = data, kwk::of_size(2,2)};
-
-
-  int count = 0;
-  auto res = reduce(kwk::cpu, d,
-  [&count](auto a, auto e)
-  {
-    count++;
-    return (a+e);
-  });
-
-  TTS_EQUAL(res,   vdata);
-
-  TTS_EQUAL(count,   d.numel());
-};
-
-TTS_CASE("Check for kwk::reduce(in, func, init) 1D")
-{
-  int data[2];
-  int vdata = 11;
-
-  fill_data(data, kwk::of_size(2), true);
-
-  auto d = kwk::view{kwk::source = data, kwk::of_size(2)};
-
-  int count = 0;
-  auto res = reduce(d,
-  [&count](auto a, auto e)
-  {
-    count++;
-    return (a+e);
-  }, 10);
-
-  TTS_EQUAL(res,   vdata);
-  TTS_EQUAL(count,   d.numel());
-};
-
-TTS_CASE("Check for kwk::reduce(in, func, init) 2D")
-{
-  int data[2*3];
-  int vdata = 136;
-
-  fill_data(data, kwk::of_size(2,3), true);
-
-  auto d = kwk::view{kwk::source = data, kwk::of_size(2,3)};
-
-  int count = 0;
-  auto res = reduce(d,
-  [&count](auto a, auto e)
-  {
-    count++;
-    return (a+e);
-  }, 100);
-
-  TTS_EQUAL(res,   vdata);
-  TTS_EQUAL(count,   d.numel());
-};
-
-TTS_CASE("Check for kwk::reduce(in, func, init) 3D")
-{
-  int data[2*3*4];
-  int vdata = 2476;
-
-  fill_data(data, kwk::of_size(2,3,4), true);
-
-  auto d = kwk::view{kwk::source = data, kwk::of_size(2,3,4)};
-
-  int count = 0;
-  auto res = reduce(d,
-  [&count](auto a, auto e)
-  {
-    count++;
-    return (a+e);
-  }, 1000);
-
-  TTS_EQUAL(res,   vdata);
-  TTS_EQUAL(count,   d.numel());
-};
-
-TTS_CASE("Check for kwk::reduce(in, func, init) 3D - with CPU context")
-{
-  int data[2*3*4];
-  int vdata = 2476;
-
-  fill_data(data, kwk::of_size(2,3,4), true);
-
-  auto d = kwk::view{kwk::source = data, kwk::of_size(2,3,4)};
-
-  int count = 0;
-  auto res = reduce(kwk::cpu, d,
-  [&count](auto a, auto e)
-  {
-    count++;
-    return (a+e);
-  }, 1000);
-
-  TTS_EQUAL(res,   vdata);
-  TTS_EQUAL(count,   d.numel());
-};
-
-TTS_CASE("Check for kwk::reduce(in, func, init) 4D")
-{
-  int data[2*3*4*5];
-  int vdata = 84040;
-
-  fill_data(data, kwk::of_size(2,3,4,5), true);
-
-  auto d = kwk::view{kwk::source = data, kwk::of_size(2,3,4,5)};
-
-  int count = 0;
-  auto res = reduce(d,
-  [&count](auto a, auto e)
-  {
-    count++;
-    return (a+e);
-  }, 10000);
-
-  TTS_EQUAL(res,   vdata);
-  TTS_EQUAL(count,   d.numel());
+  using data_type = int;
+  const std::size_t d0 = 471;
+  const std::size_t input_size = d0;
+  std::array<data_type, input_size> input;
+
+  for (std::size_t i = 0; i < input_size; ++i) { input[i] = i * 3; }
+
+  auto view_in  = kwk::view{kwk::source = input , kwk::of_size(d0)};
+
+  std::size_t count = 0;
+  auto res = kwk::reduce(view_in, [&](auto e1, auto e2) { ++count; return e1 + e2 + 1; });
+  auto res2 = kwk::reduce(view_in, [&](auto e1, auto e2) { return e1 + e2 + 1; }, 87);
+  auto res_std = std::reduce(input.begin(), input.end(), 0, [&](auto e1, auto e2) { return e1 + e2 + 1; });
+
+  TTS_EQUAL(res, res_std);
+  TTS_EQUAL(static_cast<decltype(view_in)::value_type>(res2), res_std + 87);
+  TTS_EQUAL(count, input_size);
 };
