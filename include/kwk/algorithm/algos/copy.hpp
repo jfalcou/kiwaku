08df59e9b7f3adadadba113d0ed9d20ed01ce35c
Sylvain Joube <sylvain.joube@gmail.com>
2024-09-15 17:59:35 +0200

    Algorithms : copy

--- include/kwk/algorithm/algos/copy.hpp
+++ include/kwk/algorithm/algos/copy.hpp
@@ -1,13 +1,14 @@
-//==================================================================================================
+//======================================================================================================================
 /**
   KIWAKU - Containers Well Made
   Copyright : KIWAKU Project Contributors
   SPDX-License-Identifier: BSL-1.0
 **/
-//==================================================================================================
+//======================================================================================================================
 #pragma once
 
 #include <kwk/concepts/container.hpp>
+#include <kwk/context/context.hpp>
 #include <kwk/algorithm/algos/transform.hpp>
 #include <kwk/algorithm/algos/for_each.hpp>
 #include <kwk/detail/abi.hpp>
@@ -16,15 +17,30 @@
 
 namespace kwk
 {
-  template<concepts::container Out, concepts::container In>
-  constexpr auto copy(Out& out, In&& in)
+
+  template<typename Context, concepts::container Out, concepts::container In>
+  constexpr auto copy(Context& ctx, Out& out, In const& in)
   {
-    kwk::transform([](auto in) { return in; }, out, KWK_FWD(in) );
+    kwk::transform(ctx, [](auto in) { return in; }, out, KWK_FWD(in) );
+  }
+
+  template<concepts::container Out, concepts::container In>
+  constexpr auto copy(Out& out, In const& in)
+  {
+    kwk::copy(cpu, out, in);
+  }
+
+
+  template<typename Context, typename Func, concepts::container Out, concepts::container In>
+  constexpr auto copy_if(Context& ctx, Func f, Out& out, In const& in)
+  {
+    // kwk::for_each(ctx, [&](auto... is) { if(f(in(is...))) out(is...) = KWK_FWD(in)(is...); }, out.shape() );
+    ctx.map([f](auto& o, auto const& i) { if(f(i)) o = i; }, ctx.out(out), ctx.in(in));
   }
 
   template<typename Func, concepts::container Out, concepts::container In>
-  constexpr auto copy_if(Func f, Out& out, In&& in)
+  constexpr auto copy_if(Func f, Out& out, In const& in)
   {
-    kwk::for_each([&](auto... is) { if(f(in(is...))) out(is...) = KWK_FWD(in)(is...); }, out.shape() );
+    kwk::copy_if(cpu, f, out, in);
   }
 }
---
+++ test/algorithm/algos/context/cpu/copy.cpp
@@ -0,0 +1,60 @@
+//======================================================================================================================
+/*
+  KIWAKU - Containers Well Made
+  Copyright : KIWAKU Contributors & Maintainers
+  SPDX-License-Identifier: BSL-1.0
+*/
+//======================================================================================================================
+#include <cstdlib>
+#include <kwk/algorithm/algos/copy.hpp>
+#include <kwk/container.hpp>
+#include "test.hpp"
+#include "../generic/copy.hpp"
+
+// TODO: update these tests
+
+TTS_CASE("Check for kwk::copy(out, in) 1D")
+{
+  kwk::test::copy_out_in_1D(kwk::cpu);
+};
+
+TTS_CASE("Check for kwk::copy(out, in) 2D")
+{
+  kwk::test::copy_out_in_2D(kwk::cpu);
+};
+
+TTS_CASE("Check for kwk::copy(out, in) 3D")
+{
+  kwk::test::copy_out_in_3D(kwk::cpu);
+};
+
+TTS_CASE("Check for kwk::copy(out, in) 4D")
+{
+  kwk::test::copy_out_in_4D(kwk::cpu);
+};
+
+TTS_CASE("Check for double kwk::copy(out, in) 2D")
+{
+  kwk::test::copy_out_in_2D_double(kwk::cpu);
+};
+
+TTS_CASE("Check for kwk::copy_if(func, out, in) 1D")
+{
+  kwk::test::copy_if_func_out_in_1D(kwk::cpu);
+};
+
+TTS_CASE("Check for kwk::copy_if(func, out, in) 2D")
+{
+  kwk::test::copy_if_func_out_in_2D(kwk::cpu);
+};
+
+TTS_CASE("Check for kwk::copy_if(func, out, in) 3D")
+{
+  kwk::test::copy_if_func_out_in_3D(kwk::cpu);
+};
+
+
+TTS_CASE("Check for kwk::copy_if(func, out, in) 4D")
+{
+  kwk::test::copy_if_func_out_in_4D(kwk::cpu);
+};
