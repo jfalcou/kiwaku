3d1d38deb86fd30186274b341760e17a11dea438
Sylvain Joube <sylvain.joube@gmail.com>
2025-06-03 16:02:48 +0200

    SYCL context: reduce_v2, transform_reduce and find_if_v2 + removed old code

--- include/kwk/context/sycl/context.hpp
+++ include/kwk/context/sycl/context.hpp
@@ -16,7 +16,6 @@
 #include <kwk/context/sycl/internal/sycl_tools.hpp>
 #include <cmath>
 #include <functional>
-#include <kwk/context/sycl/internal/sycl_reduce_impl.hpp>
 
 
 
@@ -88,81 +87,6 @@ namespace kwk::sycl
     }
 
 
-    template<typename Func, concepts::container In>
-    auto reduce_internal_v1(In const& in, [[maybe_unused]] Func f, [[maybe_unused]] auto init)
-    {
-      // std::cout 
-      // << "Running on device: "
-      // << parent::get_device().get_info<::sycl::info::device::name>()
-      // << "   workitem_count(" << workitem_count << ")"
-      // << "\n";
-      // std::cout << "kwk::sycl::context::reduce_internal\n";
-
-      using data_type = typename In::value_type;
-
-      std::size_t numel = in.numel();
-      // std::cout << "in.numel = " << numel << "\n";
-
-      // Number of work-items (equivalent to cuda threads)
-      std::size_t witems = workitem_count;
-
-      // std::size_t iterations_per_witem = std::ceil(numel / workitem_count);
-      // std::size_t
-
-
-      if (witems > numel) witems = numel; // avoid having more workitems than elements to process
-      ::sycl::range<1> num_items{witems};
-
-      // Simple copy from in_vec to out_vec
-      std::vector<data_type> out_vec;
-      out_vec.resize(numel);
-
-      std::fill(out_vec.begin(), out_vec.end(), 0);
-
-      // Cas simple où num_out.size = num_in.size
-
-      ::sycl::buffer out_buf(out_vec);
-      auto in_proxy = this->in(in);
-
-      // TODO 2024-02-27 : faire avec les reduction variables
-
-      parent::submit([&](::sycl::handler &h) {
-        ::sycl::accessor in_acc = in_proxy.access(h);
-        // ::sycl::accessor in_acc(in_buf, h, ::sycl::read_only);
-        ::sycl::accessor out_acc(out_buf, h, ::sycl::write_only, ::sycl::no_init);
-        h.parallel_for(num_items, [=](auto index) {
-          std::size_t gid = index[0];
-
-          // out_acc[i] = in_acc[i];
-
-          //  in case (numel < num_items)
-          if (gid < numel)
-          {
-            data_type ini = in_acc[gid];
-
-            for (std::size_t i = gid + witems; i < numel; i += witems)
-            {
-              // sum += acc_in[i];
-              // ini = f(ini, acc_in[i]);
-              ini = f(ini, in_acc[i]);
-            }
-            out_acc[gid] = ini;
-          }
-        });
-      });
-      parent::wait();
-
-      data_type res = init;
-
-      ::sycl::host_accessor h_acc(out_buf);
-      // get_host_access
-      for (std::size_t i = 0; i < witems; ++i)
-      {
-        res = f(res, h_acc[i]);
-      }
-
-      return res;
-    }
 
     // template<typename _Tp>
     // struct custom_plus : public ::std::binary_function<_Tp, _Tp, _Tp>
@@ -190,14 +114,30 @@ namespace kwk::sycl
     auto reduce_internal_v2(In const& in, [[maybe_unused]] Func func, [[maybe_unused]] auto init)
     {
       using data_type = typename In::value_type;
+      
+      data_type result = init;
+      {
+        std::size_t const numel = in.numel();
+        auto in_proxy = this->in(in);
+        ::sycl::buffer<data_type> result_buf(&result, ::sycl::range<1>(1));
 
-      std::size_t numel = in.numel();
+        parent::submit([&](::sycl::handler& h)
+        {
+          ::sycl::accessor data_acc = in_proxy.access(h);
+          auto reduction_obj = ::sycl::reduction(result_buf, h, func);
 
-      // Number of work-items (equivalent to cuda threads)
-      std::size_t witems = workitem_count;
+          h.parallel_for( ::sycl::range<1>(numel)
+                        , reduction_obj
+                        , [=](::sycl::id<1> i, auto& sum)
+                          {
+                            sum.combine(data_acc[i]);
+                          }
+                        );
+        });
+        parent::wait();
+      }
 
-
-      return sycl_reduce(*this, in, func, init);
+      return result;
     }
 
 
@@ -220,173 +160,165 @@ namespace kwk::sycl
     }
 
 
-
-    template<concepts::container Container, typename Check>
-    std::optional<kwk::position<Container::static_order>>
-    find_if(Container const& container, Check f, bool ascending)
+    template<typename Func_R, typename Func_T, concepts::container In1, concepts::container In2>
+    constexpr auto transform_reduce(In1 const& in1, In2 const& in2, auto init, Func_R R, Func_T T)
     {
-      std::size_t numel = container.numel();
+      // transform_reduce
+      // init = R(init, T(i1, i2));
 
-      std::vector<int> found_at_index{-1};
-      ::sycl::buffer b_found(found_at_index);
+      // reduce
+      // init = R(init, i);
 
-      // std::cout << "  CONTEXT SYCL - find_if - numel = " << numel << "\n";
-
-      // Always call in/out/inout outside of the submit lambda.
-      auto p_container = in(container);
-
-
-      parent::submit([&](::sycl::handler &h) 
+      try
       {
-        auto a_container = p_container.access(h);
-        ::sycl::accessor a_found(b_found, h, ::sycl::read_write);
-    
-        // find first
-        if (ascending)
+        // using data_type = typename In::value_type;
+        
+        auto result = init;
         {
-          h.parallel_for(numel, [=](auto index)
+          std::size_t const numel = in1.numel();
+          auto in1_proxy = this->in(in1);
+          auto in2_proxy = this->in(in2);
+          ::sycl::buffer<decltype(init)> result_buf(&result, ::sycl::range<1>(1));
+
+          parent::submit([&](::sycl::handler& h)
           {
-            int glob_id       = index[0];
-            int current_value = a_container[index];
+            ::sycl::accessor data1_acc = in1_proxy.access(h);
+            ::sycl::accessor data2_acc = in2_proxy.access(h);
 
-            if (f(current_value))
-            {
-              auto atomic_accessor = 
-                ::sycl::atomic_ref< int
-                                  , ::sycl::memory_order::relaxed
-                                  , ::sycl::memory_scope::device
-                                  , ::sycl::access::address_space::global_space
-                                  >(a_found[0]);
+            auto reduction_obj = ::sycl::reduction(result_buf, h, R);
 
-              // TODO: find an atomic way to do the comparison and update
-              int val = atomic_accessor.load();
-              if ((val == -1) || (val > glob_id))
-              {
-                atomic_accessor.store(glob_id);
-              }
-            }
+            h.parallel_for( ::sycl::range<1>(numel)
+                          , reduction_obj
+                          , [=](::sycl::id<1> i, auto& sum)
+                            {
+                              sum.combine(T(data1_acc[i], data2_acc[i]));
+                            }
+                          );
           });
+          parent::wait();
         }
-        else // find last
+
+        return result;
+      }
+      catch (::sycl::exception const &e)
+      {
+        std::cout << "An exception is caught for SYCL transform_reduce.\n";
+        std::terminate();
+      }
+
+      // return error for in1.shape() != in2.shape()
+      // ctx.map([&](auto const& i1, auto const& i2) { init = R(init, T(i1, i2)); }, ctx.in(in1), ctx.in(in2));
+      // return init;
+    }
+
+    template<concepts::container In, typename Func>
+    std::optional<kwk::position<In::static_order>>
+    find_if_v2(In const& in, Func func, [[maybe_unused]] bool ascending)
+    {
+      std::size_t const numel = in.numel();
+      if (numel == 0) return std::nullopt;
+
+      int result = -1;
+      try
+      {
+        // TODO: will only work if values contained in the "in" container are large enough to accept 
+        // Reduce function : take the smallest index
+
+        auto rfunc_ascending = [](int a, int b) 
         {
-          h.parallel_for(numel, [=](auto index)
+          if (a == -1) return b;
+          if (b == -1) return a;
+          if (a > b) return b;
+          else       return a;
+        };
+
+        auto rfunc_descending = [](int a, int b)
+        {
+          if (a < b) return b;
+          else       return a;
+        };
+
+        {
+
+          ::sycl::buffer<decltype(result)> result_buf(&result, ::sycl::range<1>(1));
+
+          bool* filter = ::sycl::malloc_device<bool>(numel, *(this));
+
+          auto in_proxy = this->in(in);
+          // in_proxy.discard_final_data(); // Will not copy data back to the host
+
+          parent::submit([&](::sycl::handler& h)
           {
-            int glob_id       = index[0];
-            int current_value = a_container[index];
+            ::sycl::accessor data_acc = in_proxy.access(h);
 
-            if (f(current_value))
-            {
-              auto atomic_accessor =
-                ::sycl::atomic_ref< int
-                                  , ::sycl::memory_order::relaxed
-                                  , ::sycl::memory_scope::device
-                                  , ::sycl::access::address_space::global_space
-                                  >(a_found[0]);
-
-              // TODO: find an atomic way to do the comparison and update
-              int val = atomic_accessor.load();
-              if ((val == -1) || (val < glob_id))
-              {
-                atomic_accessor.store(glob_id);
-              }
-            }
+            // First, test all values
+            h.parallel_for( ::sycl::range<1>(numel)
+                          , [=](::sycl::id<1> i)
+                            {
+                              if (func(data_acc[i[0]])) filter[i[0]] = true;
+                              else                      filter[i[0]] = false;
+                            }
+                          );
           });
+          parent::wait();
+
+          // std::cout << "Filter: ";
+          // for (std::size_t i = 0; i < numel; ++i)
+          // {
+          //   std::cout << "" << filter[i] << " ";
+          // }
+          // std::cout << "\n";
+
+          if (ascending)
+          {
+            parent::submit([&](::sycl::handler& h)
+            {
+              auto reduction_obj = ::sycl::reduction(result_buf, h, rfunc_ascending);
+
+              // Then reduce the elements
+              h.parallel_for( ::sycl::range<1>(numel)
+                            , reduction_obj
+                            , [=](::sycl::id<1> i, auto& sum)
+                              {
+                                sum.combine(filter[i[0]] ? i[0] : -1);
+                              }
+                            );
+            });
+          }
+          else
+          {
+            parent::submit([&](::sycl::handler& h)
+            {
+              auto reduction_obj = ::sycl::reduction(result_buf, h, rfunc_descending);
+
+              // Then reduce the elements
+              h.parallel_for( ::sycl::range<1>(numel)
+                            , reduction_obj
+                            , [=](::sycl::id<1> i, auto& sum)
+                              {
+                                sum.combine(filter[i[0]] ? i[0] : -1);
+                              }
+                            );
+            });
+          }
+          parent::wait();
+          ::sycl::free(filter, *this);
         }
-      });
+      }
+      catch (::sycl::exception const &e)
+      {
+        std::cout << "An exception is caught for SYCL transform_reduce.\n";
+        std::terminate();
+      }
 
-      parent::wait();
-
-      ::sycl::host_accessor hostAcc(b_found, ::sycl::read_only);
-      int index = hostAcc[0];
-
-      if (index != -1)
+      if (result != -1)
       {
         // kwk::coordinates(it, out.shape());
-        return std::optional<kwk::position<Container::static_order>>{kwk::utils::tools::linear_to_pos(index, container)};
+        return std::optional<kwk::position<In::static_order>>{kwk::utils::tools::linear_to_pos(result, in)};
       } else {
         return std::nullopt;
       }
-
     }
-
-    template<concepts::container Container, typename Check>
-    std::optional<kwk::position<Container::static_order>>
-    find_if(Container const& container, Check f)
-    {
-      return this->find_if(container, f, true);
-    }
-
-    template <typename Func, concepts::container Out>
-    constexpr std::optional<kwk::position<Out::static_order>>
-    find_last_if(Out const& out, Func f)
-    {
-      auto res = this->find_if(out, f, false);
-      return res;
-    }
-
-
-    // template<typename Func_R, typename Func_T, concepts::container In1, concepts::container In2>
-    // constexpr auto transform_reduce(In1 const& in1, In2 const& in2, auto init, Func_R R, Func_T T)
-    // {
-    //   // return error for in1.shape() != in2.shape()
-    //   ctx.transform_reduce(ctx, in1, in2, init, R, T);
-    //   map([&](auto const& i1, auto const& i2) { init = R(init, T(i1, i2)); }, ctx.in(in1), ctx.in(in2));
-    //   return init;
-
-    //   using data_type = typename In::value_type;
-
-    //   std::size_t numel = in.numel();
-
-    //   // Number of work-items (equivalent to cuda threads)
-    //   std::size_t witems = workitem_count;
-    //   if (witems > numel) witems = numel; // avoid having more workitems than elements to process
-    //   ::sycl::range<1> num_items{witems};
-
-    //   // Simple copy from in_vec to out_vec
-    //   std::vector<data_type> out_vec;
-    //   out_vec.resize(numel);
-
-    //   std::fill(out_vec.begin(), out_vec.end(), 0);
-
-    //   // Cas simple où num_out.size = num_in.size
-
-    //   ::sycl::buffer out_buf(out_vec);
-    //   auto in_proxy = this->in(in);
-
-    //   parent::submit([&](::sycl::handler &h) {
-    //     ::sycl::accessor in_acc = in_proxy.access(h);
-    //     ::sycl::accessor out_acc(out_buf, h, ::sycl::write_only, ::sycl::no_init);
-    //     h.parallel_for(num_items, [=](auto index) {
-    //       std::size_t gid = index[0];
-
-    //       //  in case (numel < num_items)
-    //       if (gid < numel)
-    //       {
-    //         data_type ini = in_acc[gid];
-
-    //         for (std::size_t i = gid + witems; i < numel; i += witems)
-    //         {
-    //           // sum += acc_in[i];
-    //           // ini = f(ini, acc_in[i]);
-    //           ini = R(ini, in_acc[i]);
-    //         }
-    //         out_acc[gid] = ini;
-    //       }
-    //     });
-    //   });
-    //   parent::wait();
-
-    //   data_type res = init;
-
-    //   ::sycl::host_accessor h_acc(out_buf);
-    //   for (std::size_t i = 0; i < witems; ++i)
-    //   {
-    //     res = R(res, h_acc[i]);
-    //   }
-
-    //   return res;
-    // }
-
 
 
   };
@@ -414,11 +346,19 @@ namespace kwk
     return ctx.reduce(in, f, init);
   }
 
+  template<typename Func_R, typename Func_T, concepts::container In1, concepts::container In2>
+  constexpr auto transform_reduce(kwk::sycl::context& ctx, In1 const& in1, In2 const& in2, auto init, Func_R R, Func_T T)
+  {
+    // std::cout << "---------------------------------- transform_reduce SYCL used !\n";
+    // return error for in1.shape() != in2.shape()
+    return ctx.transform_reduce(in1, in2, init, R, T);
+  }
+
   template<concepts::container Container, typename Check>
   std::optional<kwk::position<Container::static_order>>
   find_if(kwk::sycl::context& ctx, Container const& c, Check f)
   {
-    auto res = ctx.find_if(c, f, true);
+    auto res = ctx.find_if_v2(c, f, true);
     return res;
   }
 
@@ -426,16 +366,7 @@ namespace kwk
   constexpr std::optional<kwk::position<Out::static_order>>
   find_last_if(kwk::sycl::context& ctx, Out const& out, Func f)
   {
-    return ctx.find_last_if(out, f);
+    return ctx.find_if_v2(out, f, false);
   }
-
-  // template<typename Func_R, typename Func_T, concepts::container In1, concepts::container In2>
-  // constexpr auto transform_reduce(kwk::sycl::context& ctx, In1 const& in1, In2 const& in2, auto init, Func_R R, Func_T T)
-  // {
-  //   // return error for in1.shape() != in2.shape()
-  //   ctx.transform_reduce(ctx, in1, in2, init, R, T);
-  //   map([&](auto const& i1, auto const& i2) { init = R(init, T(i1, i2)); }, ctx.in(in1), ctx.in(in2));
-  //   return init;
-  // }
 
 }
