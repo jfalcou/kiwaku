c5c8ba33c22eb1cac913064e6306054c1c643001
Sylvain Joube <sylvain.joube@gmail.com>
2025-06-11 23:28:30 +0200

    SYCL benchmark: stable update for find and reduce

--- test/bench/find_1d.cpp
+++ test/bench/find_1d.cpp
@@ -16,6 +16,7 @@
 #include <numeric>
 #include <optional>
 #include <benchmark/benchmark.hpp>
+#include <benchmark/utils/utils.hpp>
 #include <cmath>
 
 
@@ -117,7 +118,7 @@
 //   // TTS_EQUAL(res_hand, res_hand);
 // };
 
-
+template<typename DATA_TYPE>
 void find_test( std::string const& bench_name
               , std::string const& file_name
               , auto convert_func
@@ -125,10 +126,9 @@ void find_test( std::string const& bench_name
               , std::size_t const put_at_pos
               )
 {
-  using data_type = int;
   const std::size_t d0 = view_size;
   const std::size_t input_size = d0;
-  std::vector<data_type> input;
+  std::vector<DATA_TYPE> input;
   input.resize(input_size);
 
   for (std::size_t i = 0; i < input_size; ++i) { input[i] = i * 79865 + 871 ; }
@@ -139,7 +139,7 @@ void find_test( std::string const& bench_name
   input[put_at_pos] = initial_value;
   int image_value = convert_func(initial_value);
   auto compare_func = [=](auto item) { return (convert_func(item) == image_value); };
-  
+
   int res_kwk_cpu, res_std, res_hand; // res_std_par
 
   // auto fct_kwk_sycl = [&]()
@@ -179,19 +179,18 @@ void find_test( std::string const& bench_name
   };
 
   kwk::bench::cbench_t b;
-
   std::string absolute_path = ""; // will output to "kiwaku_build"
 
-  b.start(absolute_path + file_name, bench_name);
+  b.start(absolute_path + kwk::bench::fprefix() + file_name, bench_name, "Processed elements, per second (higher is better)", view_size);
   b.set_iterations(10);
-  std::cout << "=============== std::find_if..." << std::flush;
-  b.run_function("std::find_if", fct_std);
-  std::cout << " OK!\n";
+  // std::cout << "=============== std::find_if..." << std::flush;
+  b.run_function("std::find_if, single thread on CPU", fct_std);
+  // std::cout << " OK!\n";
 
   // Don't forget -fsycl-targets=nvptx64-nvidia-cuda
   bool has_gpu = kwk::sycl::has_gpu();
 
-  auto sycl_bench = [&](auto&& context, data_type& return_) -> data_type
+  auto sycl_bench = [&](auto&& context, DATA_TYPE& return_) -> DATA_TYPE
   {
     auto fct_kwk_sycl_generic = [&]()
     {
@@ -199,9 +198,9 @@ void find_test( std::string const& bench_name
       return_ = kwk::utils::tools::pos_to_linear(view_in, pos);
       return return_;
     };
-    std::cout << "=============== find_if SYCL..." << std::flush;
+    // std::cout << "=============== find_if SYCL..." << std::flush;
     b.run_function("Kiwaku SYCL on " + context.get_device_name(), fct_kwk_sycl_generic);
-    std::cout << " OK!\n";
+    // std::cout << " OK!\n";
     return return_;
     // TTS_EQUAL(return_, res_std);
   };
@@ -209,7 +208,7 @@ void find_test( std::string const& bench_name
   // Execute SYCL benchmark on GPU and CPU
   if (has_gpu)
   {
-    data_type return_;
+    DATA_TYPE return_;
     // GPU
     sycl_bench(kwk::sycl::context{::sycl::gpu_selector_v}, return_);
     TTS_EQUAL(return_, res_std);
@@ -219,7 +218,7 @@ void find_test( std::string const& bench_name
   }
   else // SYCL default context
   {
-    data_type return_;
+    DATA_TYPE return_;
     // CPU
     sycl_bench(kwk::sycl::default_context, return_);
     TTS_EQUAL(return_, res_std);
@@ -239,43 +238,121 @@ void find_test( std::string const& bench_name
 
 TTS_CASE("Benchmark - find-if, compute-bound, last pos")
 {
-  const std::size_t d0 = 1024 * 1024 * 16; // 256
   auto convert_func = [=](auto item)
   {
+    // TODO: test with multiplications and divisions
+    // Modulo may imply thread divergeance, which would impair GPU performance.
     return
-    static_cast<int>(std::cos(std::sin(std::cos(std::sin(static_cast<float>(item * 11) * 7.) * 3.8) * 8.7) * 9.48) * 89647681.) % 1784586545;
+    static_cast<int>(
+      std::cos(
+        std::sin(
+          std::cos(
+            std::sin(
+              static_cast<float>(item * 11) * 7.
+                    ) * 3.8
+                  ) * 8.7
+                ) * 9.48
+              ) * 89647681.
+            ) % 1784586545;
   };
-  find_test("find-if compute-bound", "find-if_compute-bound_2025-06-05_01h45_last-pos.txt", convert_func, d0, d0-2);
+
+  using data_type = int;
+  [[maybe_unused]] std::size_t kio = 1024 / sizeof(data_type);
+  [[maybe_unused]] std::size_t mio = 1024 * kio;
+  [[maybe_unused]] std::size_t gio = 1024 * mio;
+
+  std::size_t size;
+  std::string hname = sutils::get_host_name();
+       if (hname == "parsys-legend")          { size = 256 * mio; } 
+  else if (hname == "pata")                   { size =  64 * mio; }
+  else if (hname == "chaton")                 { size =  64 * mio; }
+  else if (hname == "sylvain-ThinkPad-T580")  { size =   8 * mio; }
+  else                                        { size =  64 * mio; }
+
+  // const std::size_t d0 = 1024 * 1024 * 16;
+  
+  find_test<data_type>("find-if compute-bound", "find-if_compute-bound.txt", convert_func, size, size-2);
   std::cout << "\n\n";
 };
 
 
 TTS_CASE("Benchmark - find-if, memory-bound, last pos")
 {
-  const std::size_t d0 = 1024 * 1024 * 300;
+  using data_type = int;
+  [[maybe_unused]] std::size_t kio = 1024 / sizeof(data_type);
+  [[maybe_unused]] std::size_t mio = 1024 * kio;
+  [[maybe_unused]] std::size_t gio = 1024 * mio;
+
+  std::size_t size;
+  std::string hname = sutils::get_host_name();
+       if (hname == "parsys-legend")          { size =  10 * gio; } 
+  else if (hname == "pata")                   { size = 256 * mio; }
+  else if (hname == "chaton")                 { size = 256 * mio; }
+  else if (hname == "sylvain-ThinkPad-T580")  { size =  32 * mio; }
+  else                                        { size = 256 * mio; }
+
   auto convert_func = [=](auto item) { return item; };
-  find_test("find-if memory-bound", "find-if_memory-bound_2025-06-05_01h45_last-pos.txt", convert_func, d0, d0-2);
+
+  find_test<data_type>("find-if memory-bound", "find-if_memory-bound.txt", convert_func, size, size-2);
   std::cout << "\n\n";
 };
 
 
 TTS_CASE("Benchmark - find-if, compute-bound, middle")
 {
-  const std::size_t d0 = 1024 * 1024 * 16; // 256
+  using data_type = int;
+  [[maybe_unused]] std::size_t kio = 1024 / sizeof(data_type);
+  [[maybe_unused]] std::size_t mio = 1024 * kio;
+  [[maybe_unused]] std::size_t gio = 1024 * mio;
+
+  std::size_t size;
+  std::string hname = sutils::get_host_name();
+       if (hname == "parsys-legend")          { size = 256 * mio; } 
+  else if (hname == "pata")                   { size =  64 * mio; }
+  else if (hname == "chaton")                 { size =  64 * mio; }
+  else if (hname == "sylvain-ThinkPad-T580")  { size =   8 * mio; }
+  else                                        { size =  64 * mio; }
+
   auto convert_func = [=](auto item)
   {
+    // TODO: test with multiplications and divisions
+    // Modulo may imply thread divergeance, which would impair GPU performance.
     return
-    static_cast<int>(std::cos(std::sin(std::cos(std::sin(static_cast<float>(item * 11) * 7.) * 3.8) * 8.7) * 9.48) * 89647681.) % 1784586545;
+    static_cast<int>(
+      std::cos(
+        std::sin(
+          std::cos(
+            std::sin(
+              static_cast<float>(item * 11) * 7.
+                    ) * 3.8
+                  ) * 8.7
+                ) * 9.48
+              ) * 89647681.
+            ) % 1784586545;
   };
-  find_test("find-if compute-bound", "find-if_compute-bound_2025-06-05_01h45_middle.txt", convert_func, d0, d0 / 2);
+
+  find_test<data_type>("find-if compute-bound", "find-if_compute-bound_middle.txt", convert_func, size, size / 2);
   std::cout << "\n\n";
 };
 
 
 TTS_CASE("Benchmark - find-if, memory-bound, middle")
 {
-  const std::size_t d0 = 1024 * 1024 * 300;
+  using data_type = int;
+  [[maybe_unused]] std::size_t kio = 1024 / sizeof(data_type);
+  [[maybe_unused]] std::size_t mio = 1024 * kio;
+  [[maybe_unused]] std::size_t gio = 1024 * mio;
+
+  std::size_t size;
+  std::string hname = sutils::get_host_name();
+       if (hname == "parsys-legend")          { size =  10 * gio; } 
+  else if (hname == "pata")                   { size = 256 * mio; }
+  else if (hname == "chaton")                 { size = 256 * mio; }
+  else if (hname == "sylvain-ThinkPad-T580")  { size =  32 * mio; }
+  else                                        { size = 256 * mio; }
+
   auto convert_func = [=](auto item) { return item; };
-  find_test("find-if memory-bound", "find-if_memory-bound_2025-06-05_01h45_middle.txt", convert_func, d0, d0 / 2);
+
+  find_test<data_type>("find-if memory-bound", "find-if_memory-bound_middle.txt", convert_func, size, size / 2);
   std::cout << "\n\n";
 };
--- test/bench/reduce_1d.cpp
+++ test/bench/reduce_1d.cpp
@@ -12,6 +12,7 @@
 #include "test.hpp"
 #include <numeric>
 #include <benchmark/benchmark.hpp>
+#include <benchmark/utils/utils.hpp>
 // #include <execution>
 
 // TTS_CASE("Benchmark - reduce ")
@@ -134,19 +135,22 @@
 // };
 
 
+template<typename DATA_TYPE>
 void reduce_test(std::string const& bench_name, std::string const& file_name, auto func, std::size_t const view_size)
 {
-  using data_type = int;
+  // using DATA_TYPE = int;
   const std::size_t d0 = view_size;//1024 * 1024 * 256; // 256
   const std::size_t input_size = d0;
-  std::vector<data_type> input;
+  std::vector<DATA_TYPE> input;
   input.resize(input_size);
 
-  for (std::size_t i = 0; i < input_size; ++i) { input[i] = i * 3; }
+  srand(9546312);
+  const int max_number = 1 << 20;
+  for (std::size_t i = 0; i < input_size; ++i) { input[i] = rand() % max_number; }
 
   auto view_in  = kwk::view{kwk::source = input.data() , kwk::of_size(d0)};
 
-  data_type res_kwk_cpu, res_std, res_hand; // res_std_par
+  DATA_TYPE res_kwk_cpu, res_std, res_hand; // res_std_par
 
   // auto func = [](auto a, auto b) { return (((((((((a + b) % 60) % 59) % 58) % 57) % 56) % 55) % 35) % 25) % 15; };
   // auto func = [](auto a, auto b) { return (((((((((((a + b) % 4096) % 2048) % 1024) % 512) % 256) % 128) % 64) % 32) % 16) % 8); };
@@ -185,15 +189,16 @@ void reduce_test(std::string const& bench_name, std::string const& file_name, au
   // std::string absolute_path = "/home/data_evo/data_sync/academique/These/kiwaku_2025-02/test/bench/bench_files/";
   std::string absolute_path = ""; // will output so "kiwaku_build"
 
-  b.start(absolute_path + file_name, bench_name);
-  b.set_iterations(10);
-  b.run_function("std::reduce", fct_std);
+  std::string final_fname = kwk::bench::fprefix() + file_name;
+
+  b.start(absolute_path + final_fname, bench_name, "Processed elements, per second (higher is better)", view_size);
+  b.run_function("std::reduce, single thread on CPU", fct_std);
   // b.run_function("std::reduce with std::execution::par", fct_std_par);
 
   // Don't forget -fsycl-targets=nvptx64-nvidia-cuda
   bool has_gpu = kwk::sycl::has_gpu();
 
-  auto sycl_bench = [&](auto&& context, data_type& return_) -> data_type
+  auto sycl_bench = [&](auto&& context, DATA_TYPE& return_) -> DATA_TYPE
   {
     auto fct_kwk_sycl_generic = [&]()
     {
@@ -208,7 +213,7 @@ void reduce_test(std::string const& bench_name, std::string const& file_name, au
   // Execute SYCL benchmark on GPU and CPU
   if (has_gpu)
   {
-    data_type return_;
+    DATA_TYPE return_;
     // GPU
     sycl_bench(kwk::sycl::context{::sycl::gpu_selector_v}, return_);
     TTS_EQUAL(return_, res_std);
@@ -217,7 +222,7 @@ void reduce_test(std::string const& bench_name, std::string const& file_name, au
     TTS_EQUAL(return_, res_std);
 
     // // Run on GPU
-    // data_type res_kwk_sycl_gpu;
+    // DATA_TYPE res_kwk_sycl_gpu;
     // kwk::sycl::context gpu_context{::sycl::gpu_selector_v};
 
     // auto fct_kwk_sycl_gpu = [&res_kwk_sycl, &view_in, func]() {
@@ -227,7 +232,7 @@ void reduce_test(std::string const& bench_name, std::string const& file_name, au
     // b.run_function("Kiwaku SYCL GPU " + gpu_context.get_device_name(), fct_kwk_sycl_gpu);
 
     // // Run on CPU
-    // data_type res_kwk_sycl_cpu;
+    // DATA_TYPE res_kwk_sycl_cpu;
     // kwk::sycl::context cpu_context{::sycl::cpu_selector_v};
 
     // auto fct_kwk_sycl_cpu = [&res_kwk_sycl, &view_in, func]() {
@@ -238,7 +243,7 @@ void reduce_test(std::string const& bench_name, std::string const& file_name, au
   }
   else // SYCL default context
   {
-    data_type return_;
+    DATA_TYPE return_;
     // CPU
     sycl_bench(kwk::sycl::default_context, return_);
     TTS_EQUAL(return_, res_std);
@@ -254,21 +259,57 @@ void reduce_test(std::string const& bench_name, std::string const& file_name, au
 }
 
 
-// TTS_CASE("Benchmark - reduce, compute-bound ")
-// {
-//   auto func = [](auto a, auto b) { return (((((((((((a + b) % 4096) % 2048) % 1024) % 512) % 256) % 128) % 64) % 32) % 16) % 8); };
-//   const std::size_t d0 = 1024 * 1024 * 256; // 256
 
-//   reduce_test("Reduce compute-bound", "reduce_compute-bound_2025-06-04_23h11.txt", func, d0);
-// };
+
+TTS_CASE("Benchmark - reduce, compute-bound ")
+{
+  using data_type = int;
+
+  auto func = [](auto a, auto b) { return (((((((((((a + b) % 4096) % 2048) % 1024) % 512) % 256) % 128) % 64) % 32) % 16) % 8); };
+
+  // I'm writing a c++ benchmark of a reduction function. I would like to write a memory-bound version as well as a compute-bound version. I'm struggling to find a commutative and associative function that is compute intensive. Could you please help me?
+  // auto func = [](auto a, auto b) -> decltype(a) { return (std::sin(a) * 10) * (std::sin(b) * 10) + (std::cos(a) * 10) * (std::cos(b) * 10); };
+  // auto func = [](auto a, auto b) { return a + b; };
+
+  // std::cout << "COS: ";
+  // for (int i = 0; i < 100; ++i) std::cout << std::cos(i) << " ";
+  // std::cout << "\n";
+
+  [[maybe_unused]] std::size_t kio = 1024 / sizeof(data_type);
+  [[maybe_unused]] std::size_t mio = 1024 * kio;
+  [[maybe_unused]] std::size_t gio = 1024 * mio;
+  
+  std::size_t size;
+  std::string hname = sutils::get_host_name();
+       if (hname == "parsys-legend")          { size =   1 * gio; } // 4
+  else if (hname == "pata")                   { size =   1 * gio; }
+  else if (hname == "chaton")                 { size = 128 * mio; }
+  else if (hname == "sylvain-ThinkPad-T580")  { size = 128 * mio; }
+  else                                        { size =   1 * gio; }
+
+  reduce_test<data_type>("Reduce compute-bound", "reduce_compute-bound.txt", func, size);
+};
 
 
 
 
 TTS_CASE("Benchmark - reduce, memory-bound ")
 {
-  auto func = [](auto a, auto b) { return a + b; };
-  const std::size_t d0 = 1024 * 1024 * 400; // 256
+  using data_type = int;
 
-  reduce_test("Reduce memory-bound", "reduce_memory-bound_2025-06-04_23h11.txt", func, d0);
+  auto func = [](auto a, auto b) { return a + b; };
+
+  [[maybe_unused]] std::size_t kio = 1024 / sizeof(data_type);
+  [[maybe_unused]] std::size_t mio = 1024 * kio;
+  [[maybe_unused]] std::size_t gio = 1024 * mio;
+
+  std::size_t size;
+  std::string hname = sutils::get_host_name();
+       if (hname == "parsys-legend")          { size =   4 * gio; } 
+  else if (hname == "pata")                   { size =   1 * gio; }
+  else if (hname == "chaton")                 { size = 128 * mio; }
+  else if (hname == "sylvain-ThinkPad-T580")  { size = 128 * mio; }
+  else                                        { size =   1 * gio; }
+
+  reduce_test<data_type>("Reduce memory-bound", "reduce_memory-bound.txt", func, size);
 };
