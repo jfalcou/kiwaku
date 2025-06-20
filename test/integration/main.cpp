2a15bacd464fc613a145417ff2863b3983a85ead
Joel Falcou <joel.falcou@lri.fr>
2025-06-20 20:47:21 +0200

    Fix install test

--- test/integration/main.cpp
+++ test/integration/main.cpp
@@ -1,39 +1,14 @@
+#include <kwk/container.hpp>
 #include <iostream>
-#include <kumi/tuple.hpp>
-
-auto get_student(int id)
-{
-        if (id == 0)  return kumi::make_tuple(3.8, 'A', "Lisa Simpson");
-  else  if (id == 1)  return kumi::make_tuple(2.9, 'C', "Milhouse Van Houten");
-  else  if (id == 2)  return kumi::make_tuple(1.7, 'D', "Ralph Wiggum");
-  else                return kumi::make_tuple(0., 'F', "Unknown");
-}
 
 int main()
 {
-  auto student0 = get_student(0);
+  float ref[] = {1,2,3,4,5,6,7,8,9,10,11,12};
 
-  std::cout << "ID: 0, "
-            << "GPA: "    << kumi::get<0>(student0) << ", "
-            << "grade: "  << kumi::get<1>(student0) << ", "
-            << "name: "   << kumi::get<2>(student0) << '\n';
+  auto v = kwk::view{ kwk::source = ref
+                    , kwk::of_size(4,3)
+                    , kwk::label = "Test matrix"
+                    };
 
-  auto [ gpa1, grade1, name1 ] = get_student(1);
-  std::cout << "ID: 1, "
-            << "GPA: " << gpa1 << ", "
-            << "grade: " << grade1 << ", "
-            << "name: " << name1 << '\n';
-  std::cout << "\n";
-
-  auto all_students = kumi::make_tuple(get_student(0),get_student(1),get_student(2));
-
-  kumi::for_each_index( [](auto i, auto const& m) { std::cout << "Data #" << i << " : " << m << "\n";}
-                      , all_students
-                      );
-  std::cout << "\n";
-
-  auto grades = kumi::get<0>(kumi::transpose(all_students));
-  std::cout << grades << "\n";
-
-  return 0;
+  std::cout << v << "\n";
 }
