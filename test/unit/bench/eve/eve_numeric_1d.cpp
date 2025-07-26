7cecc197b5c80326ea3e7d2644513279de8999f2
Sylvain Joube <sylvain.joube@gmail.com>
2025-07-26 20:10:49 +0200

    Bench - EVE - make eve_numeric use KIWAKU CMake options

--- test/unit/bench/eve/eve_numeric_1d.cpp
+++ test/unit/bench/eve/eve_numeric_1d.cpp
@@ -5,6 +5,9 @@
   SPDX-License-Identifier: BSL-1.0
 */
 //==================================================================================================
+
+#if KIWAKU_BUILD_BENCH
+
 #include <cstdlib>
 #include <kwk/context/eve/context.hpp>
 #include <kwk/context/sycl/internal/sycl_tools.hpp>
@@ -30,6 +33,7 @@ void transform_reduce_test(std::string const& bench_name
                           , std::size_t const view_size
                           )
 {
+  std::size_t total_data_size = view_size * sizeof(DATA_TYPE) * 2;
 
   const std::size_t input_size = view_size;
   std::vector<DATA_TYPE> input1;
@@ -47,76 +51,40 @@ void transform_reduce_test(std::string const& bench_name
     input2[i] = dis(gen);
   }
 
-  // srand(89561221);
-  // const int max_number = 1 << 20;
-  // for (std::size_t i = 0; i < input_size; ++i)
-  // {
-  //   input1[i] = rand() % max_number;
-  //   input2[i] = rand() % max_number;
-  // }
-
   auto view1 = kwk::view{kwk::source = input1, kwk::of_size(input_size)};
   auto view2 = kwk::view{kwk::source = input2, kwk::of_size(input_size)};
 
 
-  DATA_TYPE res_kwk_cpu, res_kwk_simd, res_std, res_std_unseq, res_std_par, res_std_par_unseq, res_hand;
+  std::string absolute_path = ""; // will output to "kiwaku_build"
+  std::string final_fname = kwk::bench::fprefix() + file_name;
 
-  auto fct_kwk_cpu = [&]() {
-    res_kwk_cpu = kwk::transform_reduce(view1, view2, init_value, reduce_func, transform_func);
-    return res_kwk_cpu; };
+  kwk::bench::cbench_t b;
+  b.start(absolute_path + final_fname, bench_name, "Processed elements, per second (higher is better)", view_size);
 
-  auto fct_std = [&]()
+
+  // ====== Generic Kiwaku bench ======
+  // Only for kwk::cpu and kwk::sycl
+  // !! Not compatible with kwk::simd !!
+  auto bench_kiwaku = [&](auto&& context, std::string context_name)
   {
-    res_std = std::transform_reduce ( input1.begin()
-                                    , input1.end()
-                                    , input2.begin()
-                                    , init_value
-                                    , reduce_func
-                                    , transform_func
-                                    );
-    return res_std;
+    DATA_TYPE ret = -1;
+    auto fct_kwk = [&]()
+    {
+      ret = kwk::transform_reduce(context, view1, view2, init_value, reduce_func, transform_func);
+      return ret;
+    };
+    b.run_function_rpt_bwidth(context_name, 1, fct_kwk, []{}, total_data_size);
+    return ret;
   };
 
-  auto fct_std_par = [&]()
-  {
-    res_std_par = std::transform_reduce ( std::execution::par
-                                        , input1.begin()
-                                        , input1.end()
-                                        , input2.begin()
-                                        , init_value
-                                        , reduce_func
-                                        , transform_func
-                                        );
-    return res_std_par;
-  };
 
-  auto fct_std_par_unseq = [&]()
-  {
-    res_std_par_unseq = std::transform_reduce ( std::execution::par_unseq
-                                              , input1.begin()
-                                              , input1.end()
-                                              , input2.begin()
-                                              , init_value
-                                              , reduce_func
-                                              , transform_func
-                                              );
-    return res_std_par_unseq;
-  };
+  DATA_TYPE res_truth;
 
-  auto fct_std_unseq = [&]()
-  {
-    res_std_unseq = std::transform_reduce ( std::execution::unseq
-                                              , input1.begin()
-                                              , input1.end()
-                                              , input2.begin()
-                                              , init_value
-                                              , reduce_func
-                                              , transform_func
-                                              );
-    return res_std_unseq;
-  };
 
-  auto fct_hand = [&]() {
+  // ====== hand-written sequential ======
+  DATA_TYPE res_hand;
+  auto fct_hand = [&]()
+  {
     res_hand = init_value;
     for (std::size_t i = 0; i < input_size; ++i)
     {
@@ -124,149 +92,179 @@ void transform_reduce_test(std::string const& bench_name
     }
     return res_hand;
   };
-
-  std::string absolute_path = ""; // will output to "kiwaku_build"
-  std::string final_fname = kwk::bench::fprefix() + file_name;
-
-  kwk::bench::cbench_t b;
-  b.start(absolute_path + final_fname, bench_name, "Processed elements, per second (higher is better)", view_size);
-  b.run_function("std::execution::seq", fct_std); // std::transform_reduce, single thread on CPU
-  b.run_function("std::execution::unseq", fct_std_unseq); // std::transform_reduce, single thread on CPU
-  b.run_function("std::execution::par", fct_std_par);
-  b.run_function("std::execution::par_unseq", fct_std_par_unseq);
+  b.run_function_rpt_bwidth("hand CPU", 1, fct_hand, []{}, total_data_size);
+  res_truth = res_hand;
 
 
-  auto fct_kwk_simd = [&]()
+  // ====== Kiwaku CPU ======
+  DATA_TYPE res_kwk_cpu = bench_kiwaku(::kwk::cpu, "kwk CPU");
+  TTS_RELATIVE_EQUAL(res_kwk_cpu, res_truth, FLOAT_TOLERANCE_PERCENT_BENCH);
+
+
+  // ====== Kiwaku SIMD ======
+  #if KIWAKU_BENCH_EVE
+  DATA_TYPE res_kwk_simd;
+    auto fct_kwk_simd = [&]()
+    {
+      res_kwk_simd = kwk::transform_reduce( ::kwk::simd
+                                          , view1
+                                          , view2
+                                          , init_value
+                                          , std::pair{reduce_func, reduce_func_id}
+                                          , transform_func_eve
+                                          );
+      return res_kwk_simd;
+    };
+    b.run_function_rpt_bwidth("kwk simd", 1, fct_kwk_simd, []{}, total_data_size);
+    TTS_RELATIVE_EQUAL(res_kwk_simd, res_truth, FLOAT_TOLERANCE_PERCENT_BENCH);
+  #endif
+
+
+  // ====== Generic std bench ======
+  auto bench_std = [&](auto const& policy, std::string context_name)
   {
-    // std::pair{func_eve, func_id}, input[0]
-    // transform_reduce([[maybe_unused]] kwk::eve::context const& ctx, In const& in1, In const& in2, auto init, std::pair<Op, Id> R, Func_T T)
-    res_kwk_simd = kwk::transform_reduce(::kwk::simd, view1, view2, init_value, std::pair{reduce_func, reduce_func_id}, transform_func_eve);
-    return res_kwk_simd;
+    DATA_TYPE ret = -1;
+    auto fct_std = [&]()
+    {
+      ret = std::transform_reduce ( policy
+                                  , input1.begin()
+                                  , input1.end()
+                                  , input2.begin()
+                                  , init_value
+                                  , reduce_func
+                                  , transform_func
+                                  );
+      return ret;
+    };
+    b.run_function_rpt_bwidth(context_name, 1, fct_std, []{}, total_data_size);
+    return ret;
   };
-  b.run_function("Kiwaku SIMD", fct_kwk_simd);
 
-  b.run_function("Kiwaku on CPU", fct_kwk_cpu);
-  b.run_function("By hand on CPU", fct_hand);
+
+  // ====== std sequential ======
+  DATA_TYPE res_std_seq = bench_std(std::execution::seq, "seq");
+  TTS_RELATIVE_EQUAL(res_std_seq, res_truth, FLOAT_TOLERANCE_PERCENT_BENCH);
+
+
+  // ====== std unsequenced ======
+  DATA_TYPE res_std_unseq = bench_std(std::execution::unseq, "unseq");
+  TTS_RELATIVE_EQUAL(res_std_unseq, res_truth, FLOAT_TOLERANCE_PERCENT_BENCH);
+
+
+  // Don't forget the -ltbb compiler flag
+  #if ENABLE_TBB && KIWAKU_BENCH_MTHREAD
+
+    // ====== std parallel ======
+    DATA_TYPE res_std_par = bench_std(std::execution::par, "par");
+    TTS_RELATIVE_EQUAL(res_std_par, res_truth, FLOAT_TOLERANCE_PERCENT_BENCH);
+
+    // ====== std parallel unsequenced ======
+    DATA_TYPE res_std_par_unseq = bench_std(std::execution::par_unseq, "par_unseq");
+    TTS_RELATIVE_EQUAL(res_std_par_unseq, res_truth, FLOAT_TOLERANCE_PERCENT_BENCH);
+
+  #endif // ENABLE_TBB && KIWAKU_BENCH_MTHREAD
+
   b.stop();
-
-  TTS_RELATIVE_EQUAL(res_std_unseq    , res_std, FLOAT_TOLERANCE_PERCENT_BENCH);
-  TTS_RELATIVE_EQUAL(res_std_par      , res_std, FLOAT_TOLERANCE_PERCENT_BENCH);
-  TTS_RELATIVE_EQUAL(res_std_par_unseq, res_std, FLOAT_TOLERANCE_PERCENT_BENCH);
-  TTS_RELATIVE_EQUAL(res_kwk_cpu      , res_std, FLOAT_TOLERANCE_PERCENT_BENCH);
-  TTS_RELATIVE_EQUAL(res_kwk_simd     , res_std, FLOAT_TOLERANCE_PERCENT_BENCH);
-  TTS_RELATIVE_EQUAL(res_hand         , res_std, FLOAT_TOLERANCE_PERCENT_BENCH);
 }
 
 
 
-// TTS_CASE("Benchmark - transform_reduce, compute-bound ")
-// {
-//   if (::kwk::bench::enable_global)
-//   {
-//     ::kwk::bench::get_eve_compiler_flag();
+TTS_CASE("Benchmark - transform_reduce, compute-bound ")
+{
+  ::kwk::bench::get_eve_compiler_flag();
 
-//     using DATA_TYPE = float;
+  using DATA_TYPE = float;
 
-//     auto reduce_func    = [](auto a, auto b) { return a * b; };
-//     auto transform_func = [](auto a, auto b)
-//     {
-//       // cos²(a) + sin²(a) = 1
-//       // cos²(b) + sin²(b) = 1
-//       return  (
-//                 std::cos(a) * 1.86413 * std::cos(a) 
-//               + std::cos(b) * 1.86413 * std::cos(b)
-//               + std::sin(a) * 1.86413 * std::sin(a) 
-//               + std::sin(b) * 1.86413 * std::sin(b)
-//               ) / (1.86413 * 2);
-//     };
+  auto reduce_func    = [](auto a, auto b) { return a * b; };
+  auto transform_func = [](auto a, auto b)
+  {
+    // cos²(a) + sin²(a) = 1
+    // cos²(b) + sin²(b) = 1
+    return  (
+              std::cos(a) * 1.86413 * std::cos(a) 
+            + std::cos(b) * 1.86413 * std::cos(b)
+            + std::sin(a) * 1.86413 * std::sin(a) 
+            + std::sin(b) * 1.86413 * std::sin(b)
+            ) / (1.86413 * 2);
+  };
 
-//     auto transform_func_eve = [](auto a, auto b)
-//     {
-//       // cos²(a) + sin²(a) = 1
-//       // cos²(b) + sin²(b) = 1
-//       return  (
-//                 eve::cos(a) * 1.86413 * eve::cos(a) 
-//               + eve::cos(b) * 1.86413 * eve::cos(b)
-//               + eve::sin(a) * 1.86413 * eve::sin(a) 
-//               + eve::sin(b) * 1.86413 * eve::sin(b)
-//               ) / (1.86413 * 2);
-//     };
-//     // auto reduce_func    = [](auto a, auto b) { return (((((((((((a + b) % 4096) % 2048) % 1024) % 512) % 256) % 128) % 64) % 32) % 16) % 8); };
-//     // auto transform_func = [](auto a, auto b) { return ((((((((((((a + b) % 131072) % 65536) % 32768) % 16384) % 8192) % 4096) % 2048) % 1024) % 512) % 256) % 128); };
+  auto transform_func_eve = [](auto a, auto b)
+  {
+    // cos²(a) + sin²(a) = 1
+    // cos²(b) + sin²(b) = 1
+    return  (
+              eve::cos(a) * 1.86413 * eve::cos(a) 
+            + eve::cos(b) * 1.86413 * eve::cos(b)
+            + eve::sin(a) * 1.86413 * eve::sin(a) 
+            + eve::sin(b) * 1.86413 * eve::sin(b)
+            ) / (1.86413 * 2);
+  };
+  // auto reduce_func    = [](auto a, auto b) { return (((((((((((a + b) % 4096) % 2048) % 1024) % 512) % 256) % 128) % 64) % 32) % 16) % 8); };
+  // auto transform_func = [](auto a, auto b) { return ((((((((((((a + b) % 131072) % 65536) % 32768) % 16384) % 8192) % 4096) % 2048) % 1024) % 512) % 256) % 128); };
 
-//     [[maybe_unused]] std::size_t kio = 1024 / (sizeof(DATA_TYPE) * 2);
-//     [[maybe_unused]] std::size_t mio = 1024 * kio;
-//     [[maybe_unused]] std::size_t gio = 1024 * mio;
+  [[maybe_unused]] std::size_t kio = 1024 / (sizeof(DATA_TYPE) * 2);
+  [[maybe_unused]] std::size_t mio = 1024 * kio;
+  [[maybe_unused]] std::size_t gio = 1024 * mio;
 
-//     std::size_t size;
-//     std::string hname = sutils::get_host_name();
-//          if (hname == "parsys-legend")          { size =   1 * gio * kwk::bench::LEGEND_LOAD_FACTOR; } 
-//     else if (hname == "falcou-avx512")          { size =   1 * gio; }
-//     else if (hname == "pata")                   { size =   1 * gio; }
-//     else if (hname == "chaton")                 { size = 128 * mio; }
-//     else if (hname == "sylvain-ThinkPad-T580")  { size =  32 * mio; }
-//     else if (hname == "lapierre")               { size =  32 * mio; }
-//     else                                        { size =   1 * gio; }
+  std::size_t size;
+  std::string hname = sutils::get_host_name();
+        if (hname == "parsys-legend")          { size =   1 * gio * kwk::bench::LEGEND_LOAD_FACTOR; } 
+  else if (hname == "falcou-avx512")          { size =   1 * gio; }
+  else if (hname == "pata")                   { size =   1 * gio; }
+  else if (hname == "chaton")                 { size = 128 * mio; }
+  else if (hname == "sylvain-ThinkPad-T580")  { size =  32 * mio; }
+  else if (hname == "lapierre")               { size =  32 * mio; }
+  else                                        { size =   1 * gio; }
 
-//     sutils::printer_t::head("Benchmark - transform_reduce, compute-bound", true);
+  sutils::printer_t::head("Benchmark - transform_reduce, compute-bound", true);
 
-//     transform_reduce_test<DATA_TYPE>("Transform_reduce compute-bound"
-//       , "transform_reduce_compute-bound_" + kwk::bench::EVE_COMPILER_FLAG + ".bench"
-//       , reduce_func
-//       , 1 // neutral element for the reduce function
-//       , transform_func
-//       , transform_func_eve
-//       , size
-//       );
-//   }
-//   else
-//   {
-//     TTS_EQUAL(true, true);
-//   }
-// };
+  transform_reduce_test<DATA_TYPE>("Transform_reduce compute-bound"
+    , "transform_reduce_compute-bound_" + kwk::bench::EVE_COMPILER_FLAG + ".bench"
+    , reduce_func
+    , 1 // neutral element for the reduce function
+    , transform_func
+    , transform_func_eve
+    , size
+    );
+};
 
 
 
 TTS_CASE("Benchmark - transform_reduce, memory-bound ")
 {
-  if (::kwk::bench::enable_global)
-  {
-    ::kwk::bench::get_eve_compiler_flag();
-    
-    using DATA_TYPE = float;
+  ::kwk::bench::get_eve_compiler_flag();
+  
+  using DATA_TYPE = float;
 
-    auto reduce_func    = [](auto a, auto b) { return (a + b); };
-    auto transform_func = [](auto a, auto b) { return (a * b); };
+  auto reduce_func    = [](auto a, auto b) { return (a + b); };
+  auto transform_func = [](auto a, auto b) { return (a * b); };
 
-    [[maybe_unused]] std::size_t kio = 1024 / (sizeof(DATA_TYPE) + sizeof(DATA_TYPE));
-    [[maybe_unused]] std::size_t mio = 1024 * kio;
-    [[maybe_unused]] std::size_t gio = 1024 * mio;
+  [[maybe_unused]] std::size_t kio = 1024 / (sizeof(DATA_TYPE) + sizeof(DATA_TYPE));
+  [[maybe_unused]] std::size_t mio = 1024 * kio;
+  [[maybe_unused]] std::size_t gio = 1024 * mio;
 
-    std::size_t size;
-    std::string hname = sutils::get_host_name();
-          if (hname == "parsys-legend")          { size =   6 * gio * kwk::bench::LEGEND_LOAD_FACTOR; } 
+  std::size_t size;
+  std::string hname = sutils::get_host_name();
+        if (hname == "parsys-legend")          { size =   6 * gio * kwk::bench::LEGEND_LOAD_FACTOR; } 
 
-    else  if (hname == "falcou-avx512")          { size =   6 * gio; }
-    else  if (hname == "pata")                   { size =   1 * gio; }
+  else  if (hname == "falcou-avx512")          { size =   6 * gio; }
+  else  if (hname == "pata")                   { size =   1 * gio; }
 
-    else  if (hname == "chaton")                 { size = 128 * mio; }
-    else  if (hname == "sylvain-ThinkPad-T580")  { size =  32 * mio; }
-    else  if (hname == "lapierre")               { size =  32 * mio; }
-    else                                         { size =   1 * gio; }
-    
-    sutils::printer_t::head("Benchmark - transform_reduce, memory-bound", true);
+  else  if (hname == "chaton")                 { size = 128 * mio; }
+  else  if (hname == "sylvain-ThinkPad-T580")  { size =  32 * mio; }
+  else  if (hname == "lapierre")               { size =  32 * mio; }
+  else                                         { size =   1 * gio; }
+  
+  sutils::printer_t::head("Benchmark - transform_reduce, memory-bound", true);
 
-    transform_reduce_test<DATA_TYPE>("Transform_reduce memory-bound"
-                                    , "transform_reduce_memory-bound_" + kwk::bench::EVE_COMPILER_FLAG + ".bench"
-                                    , reduce_func
-                                    , 1 // neutral element for the reduce function
-                                    , transform_func
-                                    , transform_func
-                                    , size
-                                    );
-  }
-  else
-  {
-    TTS_EQUAL(true, true);
-  }
+  transform_reduce_test<DATA_TYPE>("Transform_reduce memory-bound"
+                                  , "transform_reduce_memory-bound_" + kwk::bench::EVE_COMPILER_FLAG + ".bench"
+                                  , reduce_func
+                                  , 1 // neutral element for the reduce function
+                                  , transform_func
+                                  , transform_func
+                                  , size
+                                  );
 };
+
+#endif // KIWAKU_BUILD_BENCH
