9c16c16925b039ebfbbd7fe02c6d3ed19a9136e6
Nxirda <Nxirda@users.noreply.github.com>
2025-10-22 10:46:15 +0200

    Add tests for numeric & reduce
    Enable simd tests in github workflow
    
    Test fixing eve in CI
    
    Test fixing macosx abi issue and android target
    
    update macos version to match eve

---
+++ test/unit/context/eve/reduce/2d.cpp
@@ -0,0 +1,66 @@
+//==================================================================================================
+/*
+  KIWAKU - Containers Well Made
+  Copyright : KIWAKU Contributors & Maintainers
+  SPDX-License-Identifier: BSL-1.0
+*/
+//==================================================================================================
+#include "test.hpp"
+
+#if KIWAKU_BUILD_TEST_SIMD
+
+#include <kwk/context/eve/context.hpp>
+#include <kwk/algorithm/algos/for_each.hpp>
+#include <kwk/algorithm/algos/reduce.hpp>
+#include <kwk/container.hpp>
+#include <numeric>
+
+TTS_CASE("Check for kwk::reduce(kwk::simd, in) 2D")
+{
+  using data_type = int;
+  const std::size_t d0 = 471;
+  const std::size_t d1 = 11;
+  const std::size_t input_size = d0 * d1;
+  std::array<data_type, input_size> input;
+
+  for (std::size_t i = 0; i < input_size; ++i) { input[i] = i * 3; }
+
+  auto view_in  = kwk::view{kwk::source = input , kwk::of_size(d0, d1)};
+
+  auto res = kwk::reduce(kwk::simd, view_in);
+  auto res_std = std::reduce(input.begin(), input.end());
+
+  TTS_EQUAL(res, res_std);
+};
+
+
+TTS_CASE("Check for kwk::reduce(kwk::simd, in, func) and kwk::reduce(kwk::simd, in, func, init) 2D")
+{
+  using data_type = int;
+  const std::size_t d0 = 471;
+  const std::size_t d1 = 11;
+  const std::size_t input_size = d0 * d1;
+  std::array<data_type, input_size> input;
+
+  auto monoid = std::pair{[&](auto e1, auto e2){ return e1 + e2; }, 0};
+
+  for (std::size_t i = 0; i < input_size; ++i) { input[i] = i * 3; }
+
+  auto view_in  = kwk::view{kwk::source = input , kwk::of_size(d0, d1)};
+
+  auto res = kwk::reduce(kwk::simd, view_in, monoid);
+  auto res2 = kwk::reduce(kwk::simd, view_in,monoid, 87);
+  auto res_std = std::reduce(input.begin(), input.end(), 0, [&](auto e1, auto e2) { return e1 + e2; });
+
+  TTS_EQUAL(res, res_std);
+  TTS_EQUAL(static_cast<decltype(view_in)::value_type>(res2), res_std + 87);
+};
+
+#else 
+
+TTS_CASE("EVE disabled, kwk::reduce with EVE context skipped.")
+{
+  TTS_PASS("EVE disabled, skipping test.");
+};
+
+#endif
