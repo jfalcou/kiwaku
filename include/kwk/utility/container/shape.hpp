0b6cc8dc1fa1ff3bc6f786fe61ca7549352854ac
Nxirda <93188097+Nxirda@users.noreply.github.com>
2026-03-10 14:45:19 +0100

    Use proper kumi namespace and set up precommit (#137)

--- include/kwk/utility/container/shape.hpp
+++ include/kwk/utility/container/shape.hpp
@@ -24,15 +24,16 @@
 #include <kwk/utility/slicer.hpp>
 #include <cstddef>
 
-namespace kwk::__ { struct size_; }
+namespace kwk::__
+{
+  struct size_;
+}
 
 namespace kwk
 {
-  template<auto... D>
-  struct shape;
+  template<auto... D> struct shape;
 
-  template<std::int32_t N, auto... D>
-  constexpr auto compress(shape<D...> const&) noexcept;
+  template<std::int32_t N, auto... D> constexpr auto compress(shape<D...> const&) noexcept;
 
   //====================================================================================================================
   //! @ingroup containers
@@ -73,34 +74,32 @@ namespace kwk
   //!
   //! @tparam D List of @ref glossary-extent types
   //====================================================================================================================
-  template<auto... D>
-  struct shape : __::prefilled_t<D...>::type
+  template<auto... D> struct shape : __::prefilled_t<D...>::type
   {
-    using parent        = typename __::prefilled_t<D...>::type;
-    using constraint_t  = KWK_DEFAULT_SHAPE_CONSTRAINTS;
+    using parent = typename __::prefilled_t<D...>::type;
+    using constraint_t = KWK_DEFAULT_SHAPE_CONSTRAINTS;
 
     //==================================================================================================================
     // If you end up here, you built a shape with duplicate axis names
     //==================================================================================================================
-    static_assert ( !__::prefilled_t<D...>::duplicate_axis()
-                  , "[KWK] Duplicate axis identifiers found when building shape."
-                  );
+    static_assert(!__::prefilled_t<D...>::duplicate_axis(),
+                  "[KWK] Duplicate axis identifiers found when building shape.");
 
     /// Compile-time value for @ref glossary-order
-    static constexpr std::int32_t static_order  = parent::static_size;
+    static constexpr std::int32_t static_order = parent::static_size;
 
     /// Indicates that the shape has at least one dimension specified at runtime
-    static constexpr bool is_dynamic            = parent::is_dynamic;
+    static constexpr bool is_dynamic = parent::is_dynamic;
 
     /// Indicates that the shape's dimensions are all specified at runtime
-    static constexpr bool is_fully_dynamic      = parent::is_fully_dynamic;
+    static constexpr bool is_fully_dynamic = parent::is_fully_dynamic;
 
     /// Indicates that the shape's dimensions are all specified compile-time
-    static constexpr bool is_fully_static       = parent::is_fully_static;
+    static constexpr bool is_fully_static = parent::is_fully_static;
 
     // shape is its self option keyword
     using stored_value_type = shape<D...>;
-    using keyword_type      = __::size_;
+    using keyword_type = __::size_;
 
     KWK_FORCEINLINE constexpr auto operator()(keyword_type const&) const noexcept { return *this; }
 
@@ -170,14 +169,15 @@ namespace kwk
     //! @param d  Variadic list of @ref glossary-extent.
     //==================================================================================================================
     template<concepts::extent... T>
-    requires( std::is_constructible_v<parent, int,T...> )
-    KWK_TRIVIAL explicit constexpr shape(T... d) noexcept : parent(0, d...) {}
+    requires(std::is_constructible_v<parent, int, T...>)
+    KWK_TRIVIAL explicit constexpr shape(T... d) noexcept : parent(0, d...)
+    {
+    }
 
     //==================================================================================================================
     /// Copy constructor
     //==================================================================================================================
-    KWK_TRIVIAL constexpr shape(shape const& d) noexcept : parent(d)
-    {}
+    KWK_TRIVIAL constexpr shape(shape const& d) noexcept : parent(d) {}
 
     //==================================================================================================================
     //! @brief Construct shape from another shape type
@@ -188,12 +188,12 @@ namespace kwk
     //! @param other  Shape to copy
     //==================================================================================================================
     template<auto... D2>
-    requires( constraint_t::is_contructible_from<parent{},typename shape<D2...>::parent{}>() )
+    requires(constraint_t::is_contructible_from<parent{}, typename shape<D2...>::parent{}>())
     KWK_TRIVIAL
 #if !defined(KWK_DOXYGEN_INVOKED)
-    explicit(static_order != sizeof...(D2))
+      explicit(static_order != sizeof...(D2))
 #endif
-    constexpr shape(shape<D2...> const& other) noexcept
+        constexpr shape(shape<D2...> const& other) noexcept
     {
       constraint_t::construct(*this, other);
     }
@@ -201,29 +201,26 @@ namespace kwk
     //==================================================================================================================
     /// Assignment operator
     //==================================================================================================================
-    constexpr shape& operator=( shape const& other ) & noexcept
+    constexpr shape& operator=(shape const& other) & noexcept
     {
       this->__base() = other.__base();
       return *this;
     }
 
     template<auto... D2>
-    requires( constraint_t::is_contructible_from<parent{},typename shape<D2...>::parent{}>() )
-    constexpr shape& operator=( shape<D2...> const& other ) & noexcept
+    requires(constraint_t::is_contructible_from<parent{}, typename shape<D2...>::parent{}>())
+    constexpr shape& operator=(shape<D2...> const& other) & noexcept
     {
       constraint_t::construct(*this, other);
       return *this;
     }
 
     /// Equality comparison operator
-    template<auto... D2>
-    KWK_PURE friend constexpr bool operator==(shape const& a, shape<D2...> const& b) noexcept
+    template<auto... D2> KWK_PURE friend constexpr bool operator==(shape const& a, shape<D2...> const& b) noexcept
     {
       constexpr auto sz = std::min(static_order, shape<D2...>::static_order);
       bool result = true;
-      kumi::for_each( [&](auto aa, auto bb) { result = result && (aa == bb); }
-                    , compress<sz>(a), compress<sz>(b)
-                    );
+      kumi::for_each([&](auto aa, auto bb) { result = result && (aa == bb); }, compress<sz>(a), compress<sz>(b));
       return result;
     }
 
@@ -241,7 +238,7 @@ namespace kwk
     KWK_PURE constexpr bool contains(Coords... p) const noexcept
     requires(static_order == sizeof...(Coords))
     {
-      return kumi::apply( [&](auto... m) { return ((p < static_cast<Coords>(m)) && ... && true); }, *this);
+      return kumi::apply([&](auto... m) { return ((p < static_cast<Coords>(m)) && ... && true); }, *this);
     }
 
     //==============================================================================================
@@ -250,8 +247,7 @@ namespace kwk
     //! @return `true` if both shape has the same order and if each statically set dimension of
     //!         `ref` is matched with an equal value in the current shape.
     //==============================================================================================
-    template<auto... D2>
-    KWK_PURE constexpr bool fit_constraints(shape<D2...> const& ref) const noexcept
+    template<auto... D2> KWK_PURE constexpr bool fit_constraints(shape<D2...> const& ref) const noexcept
     {
       return constraint_t::fit_constraints(*this, ref);
     }
@@ -267,7 +263,7 @@ namespace kwk
     //==================================================================================================================
     KWK_PURE constexpr std::int32_t nbdims() const noexcept
     {
-      if constexpr(static_order == 0)  return 0;
+      if constexpr (static_order == 0) return 0;
       else
       {
         auto first_non_trivial = kumi::locate(kumi::to_tuple(*this), [](auto m) { return m != 1; });
@@ -283,8 +279,8 @@ namespace kwk
     //==================================================================================================================
     KWK_PURE constexpr std::int32_t numel() const noexcept
     {
-      if constexpr(static_order == 0) return 0;
-      else return kumi::fold_left([](auto a, auto b){ return a*b; }, *this, fixed<1>);
+      if constexpr (static_order == 0) return 0;
+      else return kumi::fold_left([](auto a, auto b) { return a * b; }, *this, fixed<1>);
     }
 
     //==================================================================================================================
@@ -303,40 +299,37 @@ namespace kwk
     // https://github.com/llvm/llvm-project/issues/58952
     //==================================================================================================================
     template<typename... Slicers>
-    constexpr auto operator()(Slicers const&... s ) const noexcept
-    requires( sizeof...(Slicers) <= static_order )
+    constexpr auto operator()(Slicers const&... s) const noexcept
+    requires(sizeof...(Slicers) <= static_order)
     {
-      auto  shd     = compress<sizeof...(s)>(*this);
-      auto  sliced  = kumi::map_index ( [&](auto i, auto m) { return reshape(shd,m,i); }
-                                      , kumi::tie(s...)
-                                      );
+      auto shd = compress<sizeof...(s)>(*this);
+      auto sliced = kumi::map_index([&](auto i, auto m) { return reshape(shd, m, i); }, kumi::tie(s...));
 
-      return kumi::apply( []<typename... V>(V... v) { return shape<to_descriptor(V{})...>{v...}; }, sliced );
+      return kumi::apply([]<typename... V>(V... v) { return shape<to_descriptor(V{})...>{v...}; }, sliced);
     }
 
     /// Stream insertion operator
     friend std::ostream& operator<<(std::ostream& os, shape const& s)
     {
       os << "[";
-      kumi::for_each_index( [&](auto i, auto) { os << " " << +get<i>(s); }, s);
+      kumi::for_each_index([&](auto i, auto) { os << " " << +get<i>(s); }, s);
       return os << " ]";
     }
 
     // Access to base type for internal implementation
-    KWK_TRIVIAL constexpr auto const& __base() const  noexcept { return static_cast<parent const&>(*this);  }
-    KWK_TRIVIAL constexpr auto &      __base()        noexcept { return static_cast<parent&>(*this);        }
+    KWK_TRIVIAL constexpr auto const& __base() const noexcept { return static_cast<parent const&>(*this); }
+
+    KWK_TRIVIAL constexpr auto& __base() noexcept { return static_cast<parent&>(*this); }
   };
 
   /// Deduction guide for @ref kwk::shape
-  template<concepts::extent... T>
-  shape(T...) -> shape<to_descriptor(T{})...>;
+  template<concepts::extent... T> shape(T...) -> shape<to_descriptor(T{})...>;
 
   //====================================================================================================================
   //! @brief Generates a kwk::shape from a list of @ref glossary-extent
   //! @param  d        Variadic pack of @ref glossary-extent
   //====================================================================================================================
-  template<int..., concepts::extent... D>
-  KWK_TRIVIAL constexpr auto of_size(D... d)
+  template<int..., concepts::extent... D> KWK_TRIVIAL constexpr auto of_size(D... d)
   {
     return shape<to_descriptor(D{})...>{d...};
   }
@@ -346,8 +339,7 @@ namespace kwk
   //! @tparam SizeType  Integral type used to store sizes.
   //! @param  d         Variadic pack of @ref glossary-extent
   //====================================================================================================================
-  template<std::integral SizeType, int..., concepts::extent... D>
-  KWK_TRIVIAL constexpr auto of_size(D... d)
+  template<std::integral SizeType, int..., concepts::extent... D> KWK_TRIVIAL constexpr auto of_size(D... d)
   {
     return of_size(__::force_type<SizeType>(d)...);
   }
@@ -356,10 +348,9 @@ namespace kwk
   //! @brief Generates a kwk::shape from a tuple of @ref glossary-extent
   //! @param  t         Tuple of  @ref glossary-extent
   //====================================================================================================================
-  template<int..., kumi::product_type T>
-  KWK_TRIVIAL constexpr auto of_size(T const& t)
+  template<int..., kumi::concepts::product_type T> KWK_TRIVIAL constexpr auto of_size(T const& t)
   {
-    return kumi::apply( [](auto... e) {  return of_size(e...); }, t);
+    return kumi::apply([](auto... e) { return of_size(e...); }, t);
   }
 
   //====================================================================================================================
@@ -367,10 +358,9 @@ namespace kwk
   //! @tparam SizeType  Integral type used to store sizes.
   //! @param  t         Tuple of  @ref glossary-extent
   //====================================================================================================================
-  template<typename SizeType, int...,kumi::product_type T>
-  KWK_TRIVIAL constexpr auto of_size(T const& t)
+  template<typename SizeType, int..., kumi::concepts::product_type T> KWK_TRIVIAL constexpr auto of_size(T const& t)
   {
-    return kumi::apply( [](auto... e) {  return of_size<SizeType>(e...); }, t);
+    return kumi::apply([](auto... e) { return of_size<SizeType>(e...); }, t);
   }
 
   //====================================================================================================================
@@ -381,39 +371,38 @@ namespace kwk
   //====================================================================================================================
   template<std::int32_t N, auto... D>
   KWK_CONST constexpr auto compress(shape<D...> const& s) noexcept
-  requires(N>0 && N<=sizeof...(D))
+  requires(N > 0 && N <= sizeof...(D))
   {
     constexpr std::int32_t sz = shape<D...>::static_order;
-    if constexpr(N == sz) return s;
+    if constexpr (N == sz) return s;
     else
     {
-      auto [head,tail] = kumi::split(s, kumi::index<sz-N+1>);
-      auto value = kumi::fold_right( [](auto v, auto acc) { return acc * v; }, pop_front(head), get<0>(head));
-      return of_size(kumi::push_front(tail,value));
+      auto [head, tail] = kumi::split(s, kumi::index<sz - N + 1>);
+      auto value = kumi::fold_right([](auto v, auto acc) { return acc * v; }, pop_front(head), get<0>(head));
+      return of_size(kumi::push_front(tail, value));
     }
   }
 
   using _1D = shape<_>;
-  using _2D = shape<_,_>;
-  using _3D = shape<_,_,_>;
-  using _4D = shape<_,_,_,_>;
+  using _2D = shape<_, _>;
+  using _3D = shape<_, _, _>;
+  using _4D = shape<_, _, _, _>;
 
-//  template<std::size_t N>
-//  using _nD = []<std::size_t... I>(std::index_sequence<I...>)
-//   {
-//     return of_size(along<N-I-1>...);
-//   }(std::make_index_sequence<N>{});
+  //  template<std::size_t N>
+  //  using _nD = []<std::size_t... I>(std::index_sequence<I...>)
+  //   {
+  //     return of_size(along<N-I-1>...);
+  //   }(std::make_index_sequence<N>{});
 }
 
 #if !defined(KWK_DOXYGEN_INVOKED)
 // Tuple interface adaptation
-template<auto... D>
-struct  std::tuple_size<kwk::shape<D...>>
-      : std::integral_constant<std::int32_t,sizeof...(D)>
-{};
+template<auto... D> struct std::tuple_size<kwk::shape<D...>> : std::integral_constant<std::int32_t, sizeof...(D)>
+{
+};
 
 template<std::size_t N, auto... D>
-struct  std::tuple_element<N, kwk::shape<D...>>
-: std::tuple_element<N, typename kwk::shape<D...>::parent>
-{};
+struct std::tuple_element<N, kwk::shape<D...>> : std::tuple_element<N, typename kwk::shape<D...>::parent>
+{
+};
 #endif
--- include/kwk/utility/container/stride.hpp
+++ include/kwk/utility/container/stride.hpp
@@ -24,8 +24,7 @@ namespace kwk::__
 
 namespace kwk
 {
-  template<concepts::extent... D>
-  constexpr auto with_strides(D... d) noexcept;
+  template<concepts::extent... D> constexpr auto with_strides(D... d) noexcept;
 
   //================================================================================================
   //! @ingroup containers
@@ -63,8 +62,7 @@ namespace kwk
   //!
   //! @tparam Strides An instance of a stride descriptor
   //================================================================================================
-  template<auto... Strides>
-  struct stride  : __::prefilled_t<Strides...>::type
+  template<auto... Strides> struct stride : __::prefilled_t<Strides...>::type
   {
     using parent = typename __::prefilled_t<Strides...>::type;
 
@@ -72,21 +70,20 @@ namespace kwk
     static constexpr std::int32_t static_order = parent::static_size;
 
     /// Checks a @ref kwk::stride is unit - ie it's innermost value is statically known to be 1
-    static constexpr auto is_unit = []()
-    {
+    static constexpr auto is_unit = []() {
       auto d0 = kumi::back(parent::descriptors);
-      if constexpr(concepts::static_axis<decltype(d0)>) return d0.value == 1; else return false;
+      if constexpr (concepts::static_axis<decltype(d0)>) return d0.value == 1;
+      else return false;
     }();
 
     //==============================================================================================
     // stride is its self option keyword
     //==============================================================================================
     using stored_value_type = stride<Strides...> const&;
-    using keyword_type      = __::strides_;
+    using keyword_type = __::strides_;
 
     // constexpr auto operator()(keyword_type const&) const noexcept { return *this; }
     constexpr stored_value_type operator()(keyword_type const&) const noexcept { return *this; }
-
 
     //==============================================================================================
     //! @brief Constructs a default @ref kwk::stride equals to [1 1 ... 1]
@@ -103,14 +100,13 @@ namespace kwk
     //!
     //! @param values Variadic list of dimensions' values
     //==============================================================================================
-    constexpr stride(concepts::extent auto... values) noexcept : parent(1, values...)
-    {}
+    constexpr stride(concepts::extent auto... values) noexcept : parent(1, values...) {}
 
     /// Stream insertion operator
     friend std::ostream& operator<<(std::ostream& os, stride const& s)
     {
       os << "[";
-      kumi::for_each_index( [&](auto i, auto) { os << " " << +get<i>(s); }, s);
+      kumi::for_each_index([&](auto i, auto) { os << " " << +get<i>(s); }, s);
       return os << " ]";
     }
 
@@ -128,85 +124,73 @@ namespace kwk
     /// Indexing interface
     template<std::convertible_to<std::ptrdiff_t>... Is>
     KWK_PURE constexpr auto linearize(Is... is) const noexcept
-    requires( sizeof...(Is) == static_order )
+    requires(sizeof...(Is) == static_order)
     {
-      return [=, *this]<int...i>(std::integer_sequence<int, i...>) KWK_LAMBDA_FORCEINLINE
-      {
-          return (0 + ... + (get<i>(*this) * is));
+      return [=, *this]<int... i>(std::integer_sequence<int, i...>) KWK_LAMBDA_FORCEINLINE {
+        return (0 + ... + (get<i>(*this) * is));
       }(std::make_integer_sequence<int, static_order>{});
     }
 
     template<typename... Slicers>
-    constexpr auto operator()(Slicers const&... s ) const noexcept
-    requires( sizeof...(Slicers) == static_order )
+    constexpr auto operator()(Slicers const&... s) const noexcept
+    requires(sizeof...(Slicers) == static_order)
     {
-      auto  sliced  = kumi::map_index ( [&](auto i, auto m) { return restride(*this,m,i); }
-                                      , kumi::tie(s...)
-                                      );
+      auto sliced = kumi::map_index([&](auto i, auto m) { return restride(*this, m, i); }, kumi::tie(s...));
 
-      return kumi::apply( [](auto... v) { return with_strides(v...); }, sliced );
+      return kumi::apply([](auto... v) { return with_strides(v...); }, sliced);
     }
   };
 
   /// Deduction guide for kwk::stride
-  template<concepts::extent... T>
-  stride(T...) -> stride<to_descriptor(T{})...>;
+  template<concepts::extent... T> stride(T...) -> stride<to_descriptor(T{})...>;
 
   //================================================================================================
   //! @brief Generates a kwk::stride from a list of stride value or joker
   //! @param  d        Variadic pack of sizes
   //================================================================================================
-  template<concepts::extent... D>
-  constexpr auto with_strides(D... d) noexcept
+  template<concepts::extent... D> constexpr auto with_strides(D... d) noexcept
   {
     return stride{d...};
   }
 
-  template<kumi::product_type D>
-  constexpr auto with_strides(D d) noexcept
+  template<kumi::concepts::product_type D> constexpr auto with_strides(D d) noexcept
   {
     return kumi::apply([](auto... s) { return with_strides(s...); }, d);
   }
 
   /// Converts a kwk::shape into its corresponding kwk::stride, keeping as much static
   /// informations as possible.
-  template<auto... D>
-  KWK_PURE constexpr auto as_stride(shape<D...> s) noexcept
+  template<auto... D> KWK_PURE constexpr auto as_stride(shape<D...> s) noexcept
   {
-    if constexpr(sizeof...(D) == 1) return stride{s.template axis<0>() = fixed<1>};
+    if constexpr (sizeof...(D) == 1) return stride{s.template axis<0>() = fixed<1>};
     else
     {
-      auto const d  = kumi::fold_right ( [](auto m, auto a){ return push_front(a, m * front(a)); }
-                                      , kumi::pop_front(s)
-                                      , kumi::tuple{fixed<1>}
-                                      );
+      auto const d = kumi::fold_right([](auto m, auto a) { return push_front(a, m * front(a)); }, kumi::pop_front(s),
+                                      kumi::tuple{fixed<1>});
 
-      return [&]<std::size_t... I>(std::index_sequence<I...>, auto t)
-      {
-        return stride{ (s.template axis<I>() =  get<I>(t))... };
+      return [&]<std::size_t... I>(std::index_sequence<I...>, auto t) {
+        return stride{(s.template axis<I>() = get<I>(t))...};
       }(std::make_index_sequence<sizeof...(D)>{}, d);
     }
   }
 
-  template<std::int32_t N, auto... D>
-  constexpr auto compress(stride<D...> const& s) noexcept
+  template<std::int32_t N, auto... D> constexpr auto compress(stride<D...> const& s) noexcept
   {
-    auto extracted = kumi::extract(s, kumi::index<stride<D...>::static_size-N>);
+    auto extracted = kumi::extract(s, kumi::index<stride<D...>::static_size - N>);
     return with_strides(extracted);
   }
 }
 
 #if !defined(KWK_DOXYGEN_INVOKED)
 // Tuple interface adaptation
-template<auto... D>
-struct  std::tuple_size<kwk::stride<D...>>
-      : std::integral_constant<std::int32_t,sizeof...(D)>
-{};
+template<auto... D> struct std::tuple_size<kwk::stride<D...>> : std::integral_constant<std::int32_t, sizeof...(D)>
+{
+};
 
 template<std::size_t N, auto... D>
-struct  std::tuple_element<N, kwk::stride<D...>>
-: std::tuple_element<N, typename kwk::stride<D...>::parent>
-{};
+struct std::tuple_element<N, kwk::stride<D...>> : std::tuple_element<N, typename kwk::stride<D...>::parent>
+{
+};
 #endif
 
 #if !defined(_MSC_VER)
