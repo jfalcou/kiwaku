02294a2d9cf8965c907f2547c1fe07e24308bf67
Nxirda <Nxirda@users.noreply.github.com>
2026-03-11 10:08:22 +0100

    Proper source test

--- test/unit/settings/source.cpp
+++ test/unit/settings/source.cpp
@@ -30,7 +30,35 @@ TTS_CASE("Check if special source case behave as expected")
   auto r = std::list<int>{};
   TTS_EXPECT_NOT_COMPILES(r, { kwk::options(kwk::source = r); });
 
-  auto x = std::array<int, 10>{};
-  auto sp = std::span<int, 5>{x.data(), 5};
-  TTS_EXPECT_COMPILES(sp, { kwk::options(kwk::source = sp); });
+  auto a = std::array<int, 10>{};
+  auto f_sp = std::span<int, 5>{a.data(), 5};
+  auto v = std::vector<float>{};
+  auto d_sp = std::span<float>{v};
+  long c_array[10] = {};
+  double* ptr;
+
+  {
+    auto opt = kwk::options(kwk::source = a);
+    TTS_TYPE_IS(std::remove_cvref_t<decltype(opt[kwk::source])>, (kwk::_::array_option<int, 10>));
+  }
+  {
+    auto opt = kwk::options(kwk::source = f_sp);
+    TTS_TYPE_IS(decltype(opt[kwk::source]), (kwk::_::array_option<int, 5>));
+  }
+  {
+    auto opt = kwk::options(kwk::source = v);
+    TTS_TYPE_IS(decltype(opt[kwk::source]), (kwk::_::range_option<float>));
+  }
+  {
+    auto opt = kwk::options(kwk::source = d_sp);
+    TTS_TYPE_IS(decltype(opt[kwk::source]), (kwk::_::range_option<float>));
+  }
+  {
+    auto opt = kwk::options(kwk::source = c_array);
+    TTS_TYPE_IS(decltype(opt[kwk::source]), (kwk::_::array_option<long, 10>));
+  }
+  {
+    auto opt = kwk::options(kwk::source = ptr);
+    TTS_TYPE_IS(decltype(opt[kwk::source]), (kwk::_::pointer_option<double>));
+  }
 };
